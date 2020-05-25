b46a0f9ca3774d05376875b0987de15dc2321c7b
LionelB <lionel@lumographe.fr>
2020-05-25 16:15:14 +0200

    feat(user): create user account

--- src/pages/users/new.js
+++ src/pages/users/new.js
@@ -1,22 +1,28 @@
 /** @jsx jsx  */
-import { jsx, Heading, NavLink } from "theme-ui";
+import { jsx, Heading, NavLink, Field } from "theme-ui";
 import Head from "next/head";
 
 import { withCustomUrqlClient } from "src/components/CustomUrqlClient";
 import { withAuthProvider } from "src/hooks/useAuth";
 import { Layout } from "src/components/layout/auth.layout";
 import Link from "next/link";
+import { Button } from "src/components/button";
 
 export function UserPage() {
   return (
     <Layout>
       <Head>
-        <title>Nouvel utilisateur | Admin cdtn </title>
+        <title>Création de compte | Admin cdtn </title>
       </Head>
-      <Heading>Nouvel utilisateur</Heading>
+      <Heading>Creation de compte</Heading>
       <Link href="/users" passHref>
         <NavLink>Retour</NavLink>
       </Link>
+      <form>
+        <Field name="username" label="Nom d'utilisateur" />
+        <Field name="email" label="Adresse email" />
+        <Button>Créer un compte</Button>
+      </form>
     </Layout>
   );
 }
