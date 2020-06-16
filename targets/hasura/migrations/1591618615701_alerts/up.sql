3a56c35345ccb71e4b9c0239d084a96d26ae4f84
Douglas Duteil <douglasduteil@gmail.com>
2020-06-16 17:18:35 +0200

    chore(git): do not ignore sql files on hasura target

---
+++ targets/hasura/migrations/1591618615701_alerts/up.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE "public"."status"("name" text NOT NULL DEFAULT 'new', PRIMARY KEY ("name") );
+COMMENT ON TABLE "public"."status" IS E'alert statuses';
+
+INSERT INTO public.status (name) VALUES ('todo');
+INSERT INTO public.status (name) VALUES ('doing');
+INSERT INTO public.status (name) VALUES ('done');
+INSERT INTO public.status (name) VALUES ('rejected');
+
+CREATE TABLE "public"."sources"(
+  "repository" text NOT NULL,
+  "tag" text NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  PRIMARY KEY ("repository")
+);
+
+COMMENT ON TABLE "public"."sources" IS E'sources are git repository that acts  as data sources to track changes';
+
+INSERT INTO public.sources (repository, tag) VALUES ('socialgouv/legi-data', 'v1.9.0');
+INSERT INTO public.sources (repository, tag) VALUES ('socialgouv/kali-data', 'v1.60.0');
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE TABLE "public"."alerts"(
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "info" jsonb NOT NULL,
+  "status" text NOT NULL DEFAULT 'todo',
+  "repository" text NOT NULL,
+  "ref" text NOT NULL,
+  "changes" jsonb NOT NULL,
+  PRIMARY KEY ("id") ,
+  FOREIGN KEY ("status") REFERENCES "public"."status"("name") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("repository") REFERENCES "public"."sources"("repository") ON UPDATE restrict ON DELETE cascade); COMMENT ON TABLE "public"."alerts" IS E'alerts reprensent a change in a text from a source';
