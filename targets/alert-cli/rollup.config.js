187177bd16e70e4c43703f2aea9d0b9cb669d75b
LionelB <lionel@lumographe.fr>
2020-07-16 16:26:31 +0200

    ci(build): add rollup-plugin-json

--- targets/alert-cli/rollup.config.js
+++ targets/alert-cli/rollup.config.js
@@ -1,20 +1,22 @@
 // we use rollup to create a js bundle and thanks to the monorepo
 
-
 // transform cjs module to es6
 import commonjs from "@rollup/plugin-commonjs";
+// Converts .json files to ES6 modules.
+import json from "@rollup/plugin-json";
 // Locate and bundle third-party dependencies in node_modules
 import resolve from "@rollup/plugin-node-resolve";
 // list node builtin modules
 import builtins from "builtin-modules";
 
-const plugins = [resolve({ preferBuiltins: true }), commonjs()];
+const plugins = [json(), resolve({ preferBuiltins: true }), commonjs()];
 const external = [...builtins, "nodegit"];
 const watch = {
   clearScreen: false,
 };
 export default [
   {
+    external,
     input: `src/index.js`,
     output: [
       {
@@ -23,7 +25,6 @@ export default [
         sourcemap: true,
       },
     ],
-    external,
     plugins,
     watch,
   },
