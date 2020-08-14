dbf42988b6419e269192671144d6e4be4d9a111f
LionelB <lionel@lumographe.fr>
2020-08-14 13:49:09 +0200

    fix(alert): add git

--- targets/alert-cli/scripts/clean.sh
+++ targets/alert-cli/scripts/clean.sh
@@ -3,7 +3,7 @@
 
 for dir in `dirname "$0"`/../data/*/     # list directories in the form "/tmp/dirname/"
 do
-    dir=${dir%*/}      # remove the trailing "/"
+    dir=`dirname "$0"`/../data/${dir%*/}      # remove the trailing "/"
     echo "››› cleaning git ${dir##*/}"
     cd ${dir##*/}    # print everything after the final "/"
     git remote prune origin
