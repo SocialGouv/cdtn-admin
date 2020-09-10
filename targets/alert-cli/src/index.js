5b311fbdfb999ec8279d3f9c8a3c8d94472a1216
LionelB <lionel@lumographe.fr>
2020-09-10 10:50:59 +0200

    fix: add update source

--- targets/alert-cli/src/index.js
+++ targets/alert-cli/src/index.js
@@ -371,8 +371,8 @@ async function main() {
           );
       }
 
-      // const update = await updateSource(result.repository, result.newRef);
-      // console.log(`update source ${update.repository} to ${update.tag}`);
+      const update = await updateSource(result.repository, result.newRef);
+      console.log(`update source ${update.repository} to ${update.tag}`);
     }
   }
 }
