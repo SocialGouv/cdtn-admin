26d3eb2702c13cecc8d0c68ba2dc81c367260ea5
LionelB <lionel@lumographe.fr>
2020-09-21 10:26:09 +0200

    feat(hasura): add document table

---
+++ targets/hasura/migrations/1600080497002_create_table_public_documents/up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "public"."documents" ( "cdtn_id" text NOT NULL, "initial_id" text NOT NULL, "source" text NOT NULL, "version" text NOT NULL, "is_published" boolean NOT NULL DEFAULT TRUE, "is_searchable" boolean NOT NULL DEFAULT TRUE, "document" jsonb NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("cdtn_id"), UNIQUE ("initial_id", "source")
+);
+
+COMMENT ON TABLE "public"."documents" IS E'contains all the documents for cdtn website';
+
+CREATE TRIGGER "set_public_documents_updated_at"
+  BEFORE UPDATE ON "public"."documents"
+  FOR EACH ROW
+  EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at" ();
+
+COMMENT ON TRIGGER "set_public_documents_updated_at" ON "public"."documents" IS 'trigger to set value of column "updated_at" to current timestamp on row update';
