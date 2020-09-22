8d259bcb4777472fea3760fb937de60ca97eda7b
LionelB <lionel@lumographe.fr>
2020-09-22 15:07:50 +0200

    fix(hasura): fix procedure name for updated_at trigger

--- targets/hasura/migrations/1600080497002_documents/up.sql
+++ targets/hasura/migrations/1600080497002_documents/up.sql
@@ -20,6 +20,6 @@ COMMENT ON TABLE "public"."documents" IS E'contains all the documents for cdtn w
 CREATE TRIGGER "set_public_documents_updated_at"
   BEFORE UPDATE ON "public"."documents"
   FOR EACH ROW
-  EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at" ();
+  EXECUTE PROCEDURE trigger_set_timestamp();
 
 COMMENT ON TRIGGER "set_public_documents_updated_at" ON "public"."documents" IS 'trigger to set value of column "updated_at" to current timestamp on row update';
