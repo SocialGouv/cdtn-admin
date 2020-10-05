d8e54266fae8c26c9456da6a45eb4c5207a00046
Lionel <lionel@lumographe.fr>
2020-10-05 11:12:28 +0200

    feat(frontend): edit json document (#122)
    * feat(frontend): edit json document
    
    * fix: rename contenus
    
    * fix: change link after rename
    
    * fix: remove onChange

---
+++ targets/frontend/src/components/editor/CodeEditor.js
@@ -0,0 +1,31 @@
+/** @jsx jsx */
+
+/* eslint-disable-next-line simple-import-sort/sort */
+import PropTypes from "prop-types";
+import { jsx } from "theme-ui";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-github";
+
+export default function CodeEditor({ onChange, value }) {
+  return (
+    <AceEditor
+      mode="json"
+      theme="github"
+      name="EditJsonContent"
+      setOptions={{
+        wrap: true,
+      }}
+      editorProps={{ $blockScrolling: true }}
+      onChange={onChange}
+      value={value}
+      width="100%"
+      height="calc(100vh - 355px)"
+    />
+  );
+}
+
+CodeEditor.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
---
+++ targets/frontend/src/pages/contenus/[id].js
@@ -0,0 +1,135 @@
+/** @jsx jsx  */
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { Button } from "src/components/button";
+import { Layout } from "src/components/layout/auth.layout";
+import { Inline } from "src/components/layout/Inline";
+import { Stack } from "src/components/layout/Stack";
+import { withCustomUrqlClient } from "src/hoc/CustomUrqlClient";
+import { withUserProvider } from "src/hoc/UserProvider";
+import { Card, jsx, Message, NavLink } from "theme-ui";
+import { useMutation, useQuery } from "urql";
+
+const CodeWithCodemirror = dynamic(import("src/components/editor/CodeEditor"), {
+  ssr: false,
+});
+
+const getDocumentQuery = `
+query getDocumentById($id: String!) {
+  document: documents_by_pk(cdtn_id: $id){
+    cdtn_id,
+    document
+    initial_id
+    is_published
+    is_searchable
+    meta_description
+    slug
+    source
+    text
+    title
+  }
+}
+`;
+
+const updateDocumentMutation = `
+mutation updateDocument($cdtnId: String!, $metaDescription: String!, $title: String!, $document: jsonb!){
+  document: update_documents_by_pk(
+    _set:{
+      document: $document
+      meta_description: $metaDescription
+      title: $title
+    },
+    pk_columns: {
+      cdtn_id: $cdtnId
+    }
+  ){
+    cdtn_id
+  }
+}`;
+
+export function DocumentsPage() {
+  const router = useRouter();
+
+  const [result] = useQuery({
+    query: getDocumentQuery,
+    variables: {
+      id: router.query.id,
+    },
+  });
+  const { fetching, error, data } = result;
+
+  const [hasChanged, setHasChanged] = useState(false);
+  const [submitIdle, setSubmitIdle] = useState(false);
+  const [, executeUpdate] = useMutation(updateDocumentMutation);
+  const { handleSubmit } = useForm();
+
+  const jsonDoc = useRef(null);
+  if (!fetching && !error && jsonDoc.current === null) {
+    jsonDoc.current = data.document;
+  }
+
+  function onEditSubmit() {
+    setSubmitIdle(true);
+    return executeUpdate({
+      cdtnId: data.document.cdtn_id,
+      document: jsonDoc.current.document,
+      metaDescription: jsonDoc.current.meta_description,
+      title: jsonDoc.current.title,
+    }).then((result) => {
+      if (result.error) {
+        console.error(result.error);
+      }
+      setSubmitIdle(false);
+    });
+  }
+
+  function handleEditorChange(stringifyCode) {
+    try {
+      const newJson = JSON.parse(stringifyCode);
+      if (
+        JSON.stringify(newJson, 0, 2) !== JSON.stringify(data.document, 0, 2)
+      ) {
+        jsonDoc.current = newJson;
+        setHasChanged(true);
+      } else if (stringifyCode === JSON.stringify(data.document, 0, 2)) {
+        jsonDoc.current = data.document;
+        setHasChanged(false);
+      }
+    } catch (err) {
+      return;
+    }
+  }
+
+  if (fetching) {
+    return "...";
+  }
+
+  if (error) {
+    return <Message variant="primary">{error.message}</Message>;
+  }
+  return (
+    <Layout title={data.document.title}>
+      <form onSubmit={handleSubmit(onEditSubmit)}>
+        <Stack>
+          <Card>
+            <CodeWithCodemirror
+              value={JSON.stringify(jsonDoc.current, 0, 2)}
+              onChange={handleEditorChange}
+            />
+          </Card>
+          <Inline>
+            <Button disabled={submitIdle || !hasChanged}>Enregistrer</Button>
+            <Link href="/contenus" passHref>
+              <NavLink>Retour</NavLink>
+            </Link>
+          </Inline>
+        </Stack>
+      </form>
+    </Layout>
+  );
+}
+
+export default withCustomUrqlClient(withUserProvider(DocumentsPage));
