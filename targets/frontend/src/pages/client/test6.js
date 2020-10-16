794a72b97e40f557498fbe86ee15bda72225c920
ManuC <colasante.manuel@gmail.com>
2020-10-16 15:20:51 +0200

    allez

--- targets/frontend/src/pages/client/test6.js
+++ targets/frontend/src/pages/client/test6.js
@@ -2,12 +2,19 @@
 import { jsx } from "theme-ui";
 
 const Test6 = () => {
-  if (Math.random() > 0) {
-    // ???
-    throw new Error({ statusCode: 404 });
-  }
+  const thrower = (error) => {
+    throw new Error(error);
+  };
 
-  return <h1>Client Test 3</h1>;
+  return (
+    <>
+      <h1>Client Test 3</h1>
+      <button onClick={() => thrower({ errorCode: 404 })}>
+        Tentative de 404 on y croiiiis
+      </button>
+      <button onClick={() => thrower("bon bon")}>On se replie !</button>
+    </>
+  );
 };
 
 export default Test6;
