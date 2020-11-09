18638262b774b7e5b9fd74a880072fb107ad37d9
Lionel <lionel.breduillieard@beta.gouv.fr>
2020-11-09 17:23:38 +0100

    fix(ingester): use cdtn filtering for fiche vdd (#167)

--- targets/ingester/@types/fiches-vdd.d.ts
+++ targets/ingester/@types/fiches-vdd.d.ts
@@ -6,5 +6,21 @@ declare module "@socialgouv/fiches-vdd" {
     attributes: { ID: string, URL: string }
     children: RawJson[]
   }
+
+  export type FicheIndex = {
+    id: string
+    type: "particuliers" | "associations" | "professionnels"
+    title: string
+    subject: string
+    theme: string
+    breadcrumbs: FicheVddBreadcrumbs[]
+    date: string
+  }
+
+  export type FicheVddBreadcrumbs = {
+    id: string
+    text: string
+  }
+
   export function getFiche(type: string, id: string): RawJson
 }
