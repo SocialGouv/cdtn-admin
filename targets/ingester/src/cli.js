48973188a892da42f1a08853eaf592c0f8ac7947
Lionel <lionel.breduillieard@beta.gouv.fr>
2020-11-26 18:03:38 +0100

    feat(ingester): save ingested document version (#198)
    * update
    
    * update tables.yml
    
    * fix: rename

--- targets/ingester/src/cli.js
+++ targets/ingester/src/cli.js
@@ -42,6 +42,17 @@ mutation insert_documents($documents: [documents_insert_input!]!) {
 }
 `;
 
+const insertPackageVersionMutation = `
+mutation insert_package_version($object:package_version_insert_input!) {
+  version: insert_package_version_one(object: $object,  on_conflict: {
+    constraint: package_version_pkey,
+    update_columns: version
+  }) {
+    repository, version
+  }
+}
+`;
+
 /**
  *
  * @param {string} pkgName
@@ -119,6 +130,7 @@ async function getPackage(pkgName, pkgVersion = "latest") {
     console.debug(`download package ${pkgName}@${latest}`);
     await download(pkgName, url);
   }
+  return latest;
 }
 
 /**
@@ -163,10 +175,29 @@ async function initDocAvailabity(source) {
   }
   return result.data.documents.affected_rows;
 }
+/**
+ *
+ * @param {string} repository
+ * @param {string} version
+ */
+async function updateVersion(repository, version) {
+  const result = await client
+    .mutation(insertPackageVersionMutation, {
+      object: { repository, version },
+    })
+    .toPromise();
+  if (result.error) {
+    console.error(result.error);
+    throw new Error(`error updating package_version ${repository}@${version}`);
+  }
+  return result.data.version;
+}
 
 async function main() {
+  /** @type {{[key:string]:string}} */
+  const pkgVersions = {};
   for (const [pkgName] of dataPackages) {
-    await getPackage(pkgName);
+    pkgVersions[pkgName] = await getPackage(pkgName);
   }
   if (args.dryRun) {
     console.log("dry-run mode");
@@ -186,6 +217,7 @@ async function main() {
     const chunks = chunk(documents, 80);
     const inserts = await batchPromises(chunks, insertDocuments, 15);
     ids = ids.concat(inserts);
+    await updateVersion(pkgName, pkgVersions[pkgName]);
   }
   return ids;
 }
