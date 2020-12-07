9fe5c38bb86d26f3eb1f028716fbb1bdd08a731f
LionelB <lionel@lumographe.fr>
2020-12-07 11:02:10 +0100

    fix: revalidate after error

--- targets/frontend/src/components/button/GitlabButton.js
+++ targets/frontend/src/components/button/GitlabButton.js
@@ -1,7 +1,12 @@
 /** @jsx jsx  */
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { MdDoNotDisturbAlt, MdLoop, MdTimelapse } from "react-icons/md";
+import {
+  MdDoNotDisturbAlt,
+  MdLoop,
+  MdSyncProblem,
+  MdTimelapse,
+} from "react-icons/md";
 import { request } from "src/lib/request";
 import useSWR from "swr";
 import { jsx } from "theme-ui";
@@ -9,11 +14,20 @@ import { jsx } from "theme-ui";
 import { ConfirmButton } from "../confirmButton";
 
 export function GitlabButton({ env, children }) {
+  const [errorCount, setErrorCount] = useState(0);
   const [status, setStatus] = useState("disabled");
-  const { error, data, mutate } = useSWR("/api/pipelines");
+  const { error, data, mutate } = useSWR(() =>
+    errorCount < 3 ? "/api/pipelines" : null
+  );
 
   if (error) {
     setStatus("disabled");
+    setErrorCount(errorCount + 1);
+    setTimeout(() => mutate("/api/pipelines"), 2000);
+  }
+
+  if (errorCount >= 3) {
+    setStatus("error");
   }
 
   async function clickHandler() {
@@ -44,12 +58,13 @@ export function GitlabButton({ env, children }) {
     }
   }, [env, data, error]);
 
-  const isDisabled = status === "disabled" || status === "pending";
+  const isDisabled = status === "disabled" || status === "pending" || "error";
   return (
     <ConfirmButton disabled={isDisabled} onClick={clickHandler}>
       {status === "pending" && <MdTimelapse />}
       {status === "ready" && <MdLoop />}
       {status === "disabled" && <MdDoNotDisturbAlt />} {children}
+      {status === "error" && <MdSyncProblem />} {children}
     </ConfirmButton>
   );
 }
