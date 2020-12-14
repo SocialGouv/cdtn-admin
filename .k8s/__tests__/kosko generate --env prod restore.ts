27004a7e0a1ecb6453a008965713920a6cf5a19b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-12-14 18:14:45 +0100

    fix(ci): variable db restore options

--- .k8s/__tests__/kosko generate --env prod restore.ts
+++ .k8s/__tests__/kosko generate --env prod restore.ts
@@ -7,6 +7,9 @@ jest.setTimeout(1000 * 60);
 test("kosko generate --prod restore", async () => {
   process.env.SOURCE_CONTAINER = "source-container";
   process.env.DESTINATION_CONTAINER = "destination-container";
+  process.env.BACKUP_DB_OWNER = "some-owner";
+  process.env.BACKUP_DB_NAME = "some-database";
+  process.env.BACKUP_DB_FILE = "some-backup.sql.gz";
   expect(
     await getEnvManifests("prod", "restore", {
       ...project("cdtn-admin").prod,
