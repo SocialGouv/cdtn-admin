8ce2550b3db4a60889efa2d6bcba873399a21e1e
Lionel <lionel.breduillieard@beta.gouv.fr>
2021-01-20 12:10:03 +0100

    fix(hasura): disable email on restore (#274)

--- .k8s/components/restore/post-restore.sql
+++ .k8s/components/restore/post-restore.sql
@@ -1,5 +1,11 @@
 TRUNCATE TABLE "auth"."users" CASCADE;
 
+--
+-- DISABLE TRIGGERS
+--
+
+SET session_replication_role = REPLICA;
+
 WITH admin_row AS (
 INSERT INTO auth.users (email, PASSWORD, name, default_role, active)
     VALUES ('codedutravailnumerique@travail.gouv.fr', '$argon2i$v=19$m=4096,t=3,p=1$n9eoWSv+5sCgc7SjB5hLig$iBQ7NzrHHLkJSku/dCetNs+n/JI1CMdkWaoZsUekLU8', 'Administrateur', 'admin', TRUE)
@@ -23,3 +29,9 @@ INSERT INTO auth.users (email, PASSWORD, name, default_role, active)
     id
   FROM
     admin_row;
+
+--
+-- ENABLE TRIGGERS
+--
+
+SET session_replication_role = DEFAULT;
