bca0462faa5bca183b4ddfa362667281d9a4968b
ManuC <colasante.manuel@gmail.com>
2021-01-21 12:42:42 +0100

    wip

--- shared/id-generator/src/index.js
+++ shared/id-generator/src/index.js
@@ -1,9 +1,9 @@
-import { v4 as uuidv4 } from "uuid";
-import XXH from "xxhashjs";
+const { v4 } = require("uuid");
+const XXH = require("xxhashjs");
 
 const H = XXH.h64(0x1e7f);
 
-export const MAX_ID_LENGTH = 10;
+const MAX_ID_LENGTH = 10;
 
 // use xxhash to hash source + newly generated UUID
 /**
@@ -12,23 +12,19 @@ export const MAX_ID_LENGTH = 10;
  * @param {number} maxIdLength
  * @returns {string}
  */
-export const generateCdtnId = (content, maxIdLength = MAX_ID_LENGTH) =>
+const generateCdtnId = (content, maxIdLength = MAX_ID_LENGTH) =>
   // save 64bits hash as Hexa string up to maxIdLength chars (can be changed later in case of collision)
   // as the xxhash function ensure distribution property
   H.update(content).digest().toString(16).slice(0, maxIdLength);
 
-export const generateInitialId = uuidv4;
+const generateInitialId = v4;
 
-// Beware, you might be generating an already existing cdtn_id
-/**
- * @param {string} source
- * @param {number} maxIdLength
- * @returns {idGenerator.generatedId}
- */
-export const generateIds = (source, maxIdLength = MAX_ID_LENGTH) => {
-  const uuid = uuidv4();
-  return {
-    cdtn_id: generateCdtnId(source + uuid, maxIdLength),
-    initial_id: generateInitialId(),
-  };
+module.exports = {
+  generateIds: (source, maxIdLength = MAX_ID_LENGTH) => {
+    const uuid = v4();
+    return {
+      cdtn_id: generateCdtnId(source + uuid, maxIdLength),
+      initial_id: generateInitialId(),
+    }
+  }
 };
