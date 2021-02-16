39e86fcc4a7c84fd742cfd1d00ff07c58e2e9d53
LionelB <lionel@lumographe.fr>
2021-02-16 18:42:07 +0100

    feat(hasura): add audit tables

---
+++ targets/hasura/migrations/1613491518864_add_audit/down.sql
@@ -0,0 +1,15 @@
+DROP TRIGGER audit_trigger_row on auth.refresh_tokens;
+DROP TRIGGER audit_trigger_row on auth.user_roles;
+DROP TRIGGER audit_trigger_row on auth.users;
+DROP TRIGGER audit_trigger_row on alert_notes;
+DROP TRIGGER audit_trigger_row on alert_status;
+DROP TRIGGER audit_trigger_row on alerts;
+DROP TRIGGER audit_trigger_row on document_relations;
+DROP TRIGGER audit_trigger_row on glossary;
+DROP TRIGGER audit_trigger_row on kali_blocks;
+DROP TRIGGER audit_trigger_row on package_version;
+DROP TRIGGER audit_trigger_row on roles;
+DROP TRIGGER audit_trigger_row on sources;
+
+DROP TRIGGER documents_audit_update_selective on documents;
+DROP TRIGGER documents_audit_insert_delete on documents;
---
+++ targets/hasura/migrations/1613491518864_add_audit/up.sql
@@ -0,0 +1,31 @@
+--
+-- Audit the following table using audit triggers
+--
+
+select audit.audit_table('auth.refresh_tokens');
+select audit.audit_table('auth.user_roles');
+select audit.audit_table('auth.users');
+select audit.audit_table('alert_notes');
+select audit.audit_table('alert_status');
+select audit.audit_table('alerts');
+select audit.audit_table('document_relations');
+select audit.audit_table('glossary');
+select audit.audit_table('kali_blocks');
+select audit.audit_table('package_version');
+select audit.audit_table('roles');
+select audit.audit_table('sources');
+
+--
+-- select audit.audit_table('documents');
+-- we use audit trigger directly to take advantage of WHEN clause
+-- to log actions only if data updated since document is updated daily by the ingester,
+--
+CREATE TRIGGER documents_audit_update_selective
+  AFTER UPDATE ON documents FOR EACH ROW
+  WHEN ( (OLD.title, OLD.meta_description, OLD.slug, OLD.is_published, OLD.is_searchable, OLD.is_available)
+  IS DISTINCT FROM (NEW.title, NEW.meta_description, NEW.slug, NEW.is_published, NEW.is_searchable, NEW.is_available) )
+  EXECUTE PROCEDURE audit.if_modified_func('true');
+
+CREATE TRIGGER documents_audit_insert_delete
+  AFTER INSERT OR DELETE ON documents FOR EACH ROW
+  EXECUTE PROCEDURE audit.if_modified_func('true');
