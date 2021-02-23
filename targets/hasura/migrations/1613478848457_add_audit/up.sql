4709ea16087657f7d0625982e204baab3fdfe666
LionelB <lionel@lumographe.fr>
2021-02-23 10:59:37 +0100

    fix: review

--- targets/hasura/migrations/1613478848457_add_audit/up.sql
+++ targets/hasura/migrations/1613478848457_add_audit/up.sql
@@ -14,26 +14,7 @@ select audit.audit_table('kali_blocks'::regclass);
 select audit.audit_table('package_version'::regclass);
 select audit.audit_table('roles'::regclass);
 select audit.audit_table('sources'::regclass);
-
---
--- select audit.audit_table('documents');
--- we use audit trigger directly to take advantage of WHEN clause
--- to log update for fields other than documents, is_available, updated_at
--- since documents are updated daily by the ingester,
---
-
-CREATE TRIGGER documents_audit_update_selective
-AFTER UPDATE ON documents FOR EACH ROW
-WHEN (OLD.title IS DISTINCT FROM NEW.title
-    OR OLD.meta_description IS DISTINCT FROM NEW.meta_description
-    OR OLD.slug IS DISTINCT FROM NEW.slug
-    OR OLD.is_published IS DISTINCT FROM NEW.is_published
-    OR OLD.is_searchable IS DISTINCT FROM NEW.is_searchable)
-EXECUTE PROCEDURE audit.if_modified_func();
-
-CREATE TRIGGER documents_audit_insert_delete
-AFTER INSERT OR DELETE ON documents FOR EACH STATEMENT
-EXECUTE PROCEDURE audit.if_modified_func();
+select audit.audit_table('documents'::regclass, true, true, '{document}');
 
 --
 -- Remove old logged actions
