d5bfe5eabaa0343f4bb5c9560b253c8b4f54984b
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-29 14:55:16 +0200

    ci(gitlab): trigger alert cron

---
+++ .k8s/components/alert.cronjob.ts
@@ -0,0 +1,96 @@
+import { CronJob } from "kubernetes-models/batch/v1beta1/CronJob";
+import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
+import { merge } from "@socialgouv/kosko-charts/utils/@kosko/env/merge";
+import { PersistentVolumeClaim } from "kubernetes-models/v1/PersistentVolumeClaim";
+
+const gitlabEnv = gitlab(process.env);
+const name = "alert";
+
+const tag = process.env.CI_COMMIT_TAG
+  ? process.env.CI_COMMIT_TAG.slice(1)
+  : process.env.CI_COMMIT_SHA;
+
+const cronJob = new CronJob({
+  metadata: {
+    annotations: gitlabEnv.annotations,
+    labels: merge(
+      {
+        app: name,
+      },
+      gitlabEnv.labels ?? {}
+    ),
+    name,
+  },
+  spec: {
+    concurrencyPolicy: "Forbid",
+    successfulJobsHistoryLimit: 3,
+    failedJobsHistoryLimit: 3,
+    schedule: "0 1 * * *",
+    jobTemplate: {
+      spec: {
+        backoffLimit: 0,
+        template: {
+          spec: {
+            containers: [
+              {
+                name: "update-alert",
+                image: `${process.env.CI_REGISTRY_IMAGE}/${name}:${tag}`,
+                resources: {
+                  requests: {
+                    cpu: "1500m",
+                    memory: "2.5Gi",
+                  },
+                  limits: {
+                    cpu: "2000m",
+                    memory: "3Gi",
+                  },
+                },
+                workingDir: "/app",
+                env: [
+                  {
+                    name: "PRODUCTION",
+                    value: process.env.PRODUCTION,
+                  },
+                  {
+                    name: "HASURA_GRAPHQL_ENDPOINT",
+                    value: "http://hasura/v1/graphql",
+                  },
+                ],
+                envFrom: [
+                  {
+                    secretRef: {
+                      name: "cdtn-admin-secrets",
+                    },
+                  },
+                ],
+                volumeMounts: [
+                  {
+                    name: "tz-paris",
+                    mountPath: "/etc/localtime",
+                  },
+                ],
+              },
+            ],
+            securityContext: {
+              runAsNonRoot: true,
+              runAsUser: 1000,
+              fsGroup: 1000,
+              supplementalGroups: [1000],
+            },
+            volumes: [
+              {
+                name: "tz-paris",
+                hostPath: {
+                  path: "/usr/share/zoneinfo/Europe/Paris",
+                },
+              },
+            ],
+            restartPolicy: "Never",
+          },
+        },
+      },
+    },
+  },
+});
+
+export default [cronJob];
