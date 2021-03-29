6961c37fa98f6fdcad76d93c13e909049de855be
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-29 22:11:44 +0200

    build(k8s): correct restore dev

--- .k8s/__tests__/generate-dev-restore.ts
+++ .k8s/__tests__/generate-dev-restore.ts
@@ -4,9 +4,9 @@ import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
 import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
-test("kosko generate --prod jobs/restore", async () => {
+test("kosko generate --dev jobs/restore", async () => {
   expect(
-    await getEnvManifests("prod", "jobs/restore", {
+    await getEnvManifests("dev", "jobs/restore", {
       ...project("cdtn-admin").dev,
     })
   ).toMatchSnapshot();
--- .k8s/components/jobs/restore/index.ts
+++ .k8s/components/jobs/restore/index.ts
@@ -1,4 +1,4 @@
 import containerJob from "./container";
 import dbJob from "./db";
 import env from "@kosko/env";
-export default env.env === "prod" ? [containerJob, dbJob] : [];
+export default process.env.PRODUCTION ? [] : [containerJob, dbJob];
