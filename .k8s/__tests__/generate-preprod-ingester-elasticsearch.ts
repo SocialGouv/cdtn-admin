b47dd893a5383989f57220a8ceecaaae1bf67d6d
Douglas DUTEIL <douglasduteil@gmail.com>
2021-03-31 17:06:53 +0200

    build(k8s): update ingester-elasticsearch prod secrets

--- .k8s/__tests__/generate-preprod-ingester-elasticsearch.ts
+++ .k8s/__tests__/generate-preprod-ingester-elasticsearch.ts
@@ -4,9 +4,9 @@ import { getEnvManifests } from "@socialgouv/kosko-charts/testing";
 import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
-test("kosko generate --env dev jobs/ingester-elasticsearch", async () => {
+test("kosko generate --env preprod jobs/ingester-elasticsearch", async () => {
   expect(
-    await getEnvManifests("dev", "jobs/ingester-elasticsearch", {
+    await getEnvManifests("preprod", "jobs/ingester-elasticsearch", {
       ...project("cdtn-admin").dev,
       RANCHER_PROJECT_ID: "c-bar:p-foo",
     })
