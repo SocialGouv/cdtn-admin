4890b272780382264a78f9307ff7c2794de56e7f
LionelB <lionel@lumographe.fr>
2021-03-31 18:09:36 +0200

    fix

--- targets/frontend/src/pages/api/actions/pipelines.js
+++ targets/frontend/src/pages/api/actions/pipelines.js
@@ -1,4 +1,4 @@
-import Boom, { boomify } from "@hapi/boom";
+import Boom from "@hapi/boom";
 import { apiError } from "src/lib/apiError";
 import { getPipelines, getPipelineVariables } from "src/lib/gitlab.api";
 
