fa52ce45d89167093be3e2a3a55f35229b54ca91
LionelB <lionel@lumographe.fr>
2021-04-01 18:54:12 +0200

    fix(k8s) update es index prefix

--- .k8s/utils/ES_INDEX_PREFIX.ts
+++ .k8s/utils/ES_INDEX_PREFIX.ts
@@ -1,17 +1,3 @@
-let ES_INDEX_PREFIX =
-  process.env.ES_INDEX_PREFIX ??
-  `cdtn-feature-${process.env.CI_ENVIRONMENT_SLUG}`;
-
-if (process.env.CI_COMMIT_REF_SLUG === "master") {
-  ES_INDEX_PREFIX = "cdtn-master";
-}
-
-if (process.env.CI_COMMIT_TAG) {
-  ES_INDEX_PREFIX = `cdtn-preprod-${process.env.CI_COMMIT_TAG}`;
-}
-
-if (process.env.PRODUCTION) {
-  ES_INDEX_PREFIX = `cdtn-prod-${process.env.CI_COMMIT_TAG}`;
-}
+let ES_INDEX_PREFIX = process.env.ES_INDEX_PREFIX ?? `cdtn-master`;
 
 export { ES_INDEX_PREFIX };
