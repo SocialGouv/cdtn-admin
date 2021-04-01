c56e68e8a72b002f4c59891a88823a9a88d5d5a4
Douglas DUTEIL <douglasduteil@gmail.com>
2021-04-01 11:25:09 +0200

    build(k8s): add sitemap-uploader secrets

--- .k8s/components/jobs/sitemap-uploader/index.ts
+++ .k8s/components/jobs/sitemap-uploader/index.ts
@@ -2,6 +2,9 @@ import koskoEnv from "@kosko/env";
 import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
 import { Job } from "kubernetes-models/batch/v1/Job";
 import { merge } from "@socialgouv/kosko-charts/utils/@kosko/env/merge";
+import type { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1/SealedSecret";
+import { ok } from "assert";
+import { loadYaml } from "@socialgouv/kosko-charts/utils/getEnvironmentComponent";
 
 // renovate: datasource=docker depName=mcr.microsoft.com/azure-cli versioning=2.9.1
 const AZ_DOCKER_TAG = "2.9.1";
@@ -27,75 +30,69 @@ fi
 const gitlabEnv = gitlab(process.env);
 const env = koskoEnv.component("sitemap-uploader");
 
-const createSitemapJob = () => {
-  const job = new Job({
-    metadata: {
-      annotations: merge(gitlabEnv.annotations || {}, {
-        "kapp.k14s.io/disable-default-ownership-label-rules": "",
-        "kapp.k14s.io/disable-default-label-scoping-rules": "",
-        "kapp.k14s.io/update-strategy": "fallback-on-replace",
-      }),
-      name: `sitemap-uploader`,
-    },
+const secret = loadYaml<SealedSecret>(
+  koskoEnv,
+  "sitemap-uploader/sealed-secret.yaml"
+);
+ok(secret, "Missing sitemap-uploader/sealed-secret.yaml");
 
-    spec: {
-      backoffLimit: 3,
-      template: {
-        metadata: {
-          name: `sitemap-uploader`,
-          annotations: {
-            "kapp.k14s.io/deploy-logs": "for-new-or-existing",
-          },
-        },
-        spec: {
-          restartPolicy: "OnFailure",
-          containers: [
-            {
-              name: "az-sitemap-uploader",
-              image: `mcr.microsoft.com/azure-cli:${AZ_DOCKER_TAG}`,
-              command: ["bash"],
-              args: ["-c", uploadSitemapScript],
-              env: [
-                {
-                  name: "BASE_URL",
-                  value: process.env.BASE_URL || env.BASE_URL,
-                },
-                {
-                  name: "DESTINATION_CONTAINER",
-                  value: "sitemap",
-                },
-                {
-                  name: "DESTINATION_NAME",
-                  value: process.env.DESTINATION_NAME || env.DESTINATION_NAME,
-                },
-                {
-                  name: "SITEMAP_ENDPOINT",
-                  value: `${process.env.CI_ENVIRONMENT_URL}/api/sitemap`,
-                },
-                {
-                  name: "AZ_ACCOUNT_NAME",
-                  value: "$(azurestorageaccountname)",
-                },
-                {
-                  name: "AZ_ACCOUNT_KEY",
-                  value: "$(azurestorageaccountkey)",
-                },
-              ],
-              envFrom: [
-                {
-                  secretRef: {
-                    name: process.env.SECRET_NAME || env.SECRET_NAME,
-                  },
-                },
-              ],
-            },
-          ],
+const job = new Job({
+  metadata: {
+    annotations: merge(gitlabEnv.annotations || {}, {
+      "kapp.k14s.io/disable-default-ownership-label-rules": "",
+      "kapp.k14s.io/disable-default-label-scoping-rules": "",
+      "kapp.k14s.io/update-strategy": "fallback-on-replace",
+    }),
+    name: `sitemap-uploader`,
+  },
+
+  spec: {
+    backoffLimit: 3,
+    template: {
+      metadata: {
+        name: `sitemap-uploader`,
+        annotations: {
+          "kapp.k14s.io/deploy-logs": "for-new-or-existing",
         },
       },
+      spec: {
+        restartPolicy: "OnFailure",
+        containers: [
+          {
+            name: "az-sitemap-uploader",
+            image: `mcr.microsoft.com/azure-cli:${AZ_DOCKER_TAG}`,
+            command: ["bash"],
+            args: ["-c", uploadSitemapScript],
+            env: [
+              {
+                name: "BASE_URL",
+                value: process.env.BASE_URL || env.BASE_URL,
+              },
+              {
+                name: "DESTINATION_CONTAINER",
+                value: "sitemap",
+              },
+              {
+                name: "DESTINATION_NAME",
+                value: process.env.DESTINATION_NAME || env.DESTINATION_NAME,
+              },
+              {
+                name: "SITEMAP_ENDPOINT",
+                value: `${process.env.CI_ENVIRONMENT_URL}/api/sitemap`,
+              },
+            ],
+            envFrom: [
+              {
+                secretRef: {
+                  name: secret.metadata?.name,
+                },
+              },
+            ],
+          },
+        ],
+      },
     },
-  });
-  return job;
-};
+  },
+});
 
-const job = createSitemapJob();
-export default job;
+export default [job, secret];
