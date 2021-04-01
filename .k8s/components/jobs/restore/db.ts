a249a77b94aa14df1973ab511ff2a08802343a4f
Douglas Duteil <douglasduteil@gmail.com>
2021-04-02 00:13:48 +0200

    build(k8s): make the restore db use preprod db on preprod (#395)

--- .k8s/components/jobs/restore/db.ts
+++ .k8s/components/jobs/restore/db.ts
@@ -7,13 +7,26 @@ import { EnvVar } from "kubernetes-models/v1/EnvVar";
 import { Job } from "kubernetes-models/batch/v1/Job";
 import path from "path";
 import { GITLAB_LIKE_ENVIRONMENT_SLUG } from "../../../utils/GITLAB_LIKE_ENVIRONMENT_SLUG";
-import { PG_ENVIRONMENT_SLUG } from "../../../utils/PG_ENVIRONMENT_SLUG";
+import {
+  PG_ENVIRONMENT_SLUG,
+  PREPROD_PG_ENVIRONMENT,
+} from "../../../utils/PG_ENVIRONMENT_SLUG";
 import { getDevDatabaseParameters } from "@socialgouv/kosko-charts/components/azure-pg/params";
 import { getDefaultPgParams } from "@socialgouv/kosko-charts/components/azure-pg";
 import gitlab from "@socialgouv/kosko-charts/environments/gitlab";
 
 const suffix = PG_ENVIRONMENT_SLUG;
-const pgParams = getDevDatabaseParameters({ suffix });
+const pgParams =
+  PG_ENVIRONMENT_SLUG === PREPROD_PG_ENVIRONMENT
+    ? {
+        database: PG_ENVIRONMENT_SLUG,
+        // NOTE(douglasduteil): add fake database password here
+        // We add a "fake" password to make typescript happy
+        password: PG_ENVIRONMENT_SLUG,
+        user: PG_ENVIRONMENT_SLUG,
+      }
+    : getDevDatabaseParameters({ suffix });
+
 const gitlabEnv = gitlab(process.env);
 
 const manifests = restoreDbJob({
@@ -33,7 +46,8 @@ const manifests = restoreDbJob({
   ],
   postRestoreScript: fs
     .readFileSync(path.join(__dirname, "./post-restore.sql"))
-    .toString(),
+    .toString()
+    .replace("${PGDATABASE}", pgParams.database),
   project: "cdtn-admin",
 });
 
@@ -77,5 +91,17 @@ const pgPasswordEnvVar = initContainer.env?.find(
 );
 ok(pgPasswordEnvVar, "Missing PGPASSWORD variable");
 pgPasswordEnvVar.value = pgParams.password;
+
+if (PG_ENVIRONMENT_SLUG === PREPROD_PG_ENVIRONMENT) {
+  // prerpod configuration !
+  initContainer.env = [];
+  initContainer.envFrom = [
+    {
+      secretRef: {
+        name: "azure-pg-admin-user-dev",
+      },
+    },
+  ];
+}
 
 export default manifests;
