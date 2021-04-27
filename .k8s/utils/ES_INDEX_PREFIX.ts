7437ff9074822a46c43ab0aa3f1e8c6290bd2627
Lionel <lionel.breduillieard@beta.gouv.fr>
2021-04-27 10:23:08 +0200

    fix(ingester-es): add version on branch index name (#423)
    * fix(ingester-es): add version on branch index name
    
    * Update .k8s/utils/ES_INDEX_PREFIX.ts
    
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>
    
    * fix(ingester-es): use process.env.ES_INDEX_PREFIX i exists
    
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>

--- .k8s/utils/ES_INDEX_PREFIX.ts
+++ .k8s/utils/ES_INDEX_PREFIX.ts
@@ -1,9 +1,11 @@
-let ES_INDEX_PREFIX =
-  process.env.ES_INDEX_PREFIX ??
-  `cdtn-feature-${process.env.CI_ENVIRONMENT_SLUG}`;
+let ES_INDEX_PREFIX = `cdtn-feature-${process.env.CI_ENVIRONMENT_SLUG}`;
 
 if (process.env.CI_COMMIT_REF_SLUG === "master") {
-  ES_INDEX_PREFIX = "cdtn-master";
+  ES_INDEX_PREFIX = `cdtn-master`;
+}
+
+if (process.env.ES_INDEX_PREFIX) {
+  ES_INDEX_PREFIX = process.env.ES_INDEX_PREFIX;
 }
 
 const target = process.env.INGESTER_ELASTICSEARCH_TARGET;
