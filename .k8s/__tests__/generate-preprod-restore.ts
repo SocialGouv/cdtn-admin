cc2e11ab4d5e9d2125d4b418ad68363a0ed3d757
Douglas DUTEIL <douglasduteil@gmail.com>
2021-06-28 21:51:21 +0200

    fix(k8s): allow retore db through azure file share pv

--- .k8s/__tests__/generate-preprod-restore.ts
+++ .k8s/__tests__/generate-preprod-restore.ts
@@ -6,6 +6,7 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 jest.setTimeout(1000 * 60);
 test("kosko generate --preprod jobs/restore", async () => {
   process.env.HARBOR_PROJECT = "cdtn";
+  process.env.CI_JOB_ID = "123456789";
   expect(
     await getEnvManifests("preprod", "jobs/restore", {
       ...project("cdtn-admin").preprod,
