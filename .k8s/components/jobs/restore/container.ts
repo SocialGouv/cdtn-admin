32ebb0ab5c60e25461469c462781f39bee3d633f
Douglas DUTEIL <douglasduteil@gmail.com>
2021-08-25 19:56:05 +0200

    build(k8s): migrate to @socialgouv/kosko-charts to v9.3.3

--- .k8s/components/jobs/restore/container.ts
+++ .k8s/components/jobs/restore/container.ts
@@ -1,10 +1,6 @@
 import { restoreContainerJob } from "@socialgouv/kosko-charts/components/azure-storage/restore-container.job";
-import environments from "@socialgouv/kosko-charts/environments";
-import { ok } from "assert";
 import { EnvVar } from "kubernetes-models/v1";
 import env from "@kosko/env";
-
-const envParams = environments(process.env);
 
 const params = env.component("container");
 
@@ -23,17 +19,4 @@ const job = restoreContainerJob({
   project: "cdtn-admin",
   to: params.server,
 });
-ok(job.metadata, "Missing job metadata");
-job.metadata.name = `restore-container-${envParams.branchSlug}`;
-job.metadata.labels = envParams.metadata.labels || {};
-job.metadata.labels.component =
-  process.env.COMPONENT || `restore-${envParams.branchSlug}`;
-job.metadata.annotations = {
-  "kapp.k14s.io/update-strategy": "always-replace",
-};
-ok(job.spec, "Missing job spec");
-ok(job.spec.template!.metadata, "Missing job spec template metadata");
-job.spec.template.metadata.annotations = {
-  "kapp.k14s.io/deploy-logs": "for-new-or-existing",
-};
 export default job;
--- .k8s/components/jobs/restore/db.ts
+++ .k8s/components/jobs/restore/db.ts
@@ -1,22 +1,13 @@
 //
 
-import {
-  getDefaultPgParams,
-  PREPROD_PG_ENVIRONMENT,
-} from "@socialgouv/kosko-charts/components/azure-pg";
+import { getDefaultPgParams } from "@socialgouv/kosko-charts/components/azure-pg";
 import { restoreDbJob } from "@socialgouv/kosko-charts/components/azure-pg/restore-db.job";
-import environments from "@socialgouv/kosko-charts/environments";
-import { ok } from "assert";
 import fs from "fs";
-import { Job } from "kubernetes-models/batch/v1";
 import { EnvVar } from "kubernetes-models/v1";
-import { ObjectMeta } from "kubernetes-models/apimachinery/pkg/apis/meta/v1";
 
 import path from "path";
 
 const pgParams = getDefaultPgParams();
-
-const envParams = environments(process.env);
 
 const manifests = restoreDbJob({
   env: [
@@ -39,23 +30,4 @@ const manifests = restoreDbJob({
     .replace("${PGDATABASE}", pgParams.database),
   project: "cdtn-admin",
 });
-
-(manifests as any as { metadata: ObjectMeta }[]).forEach((m) => {
-  m.metadata = m.metadata || new ObjectMeta({});
-  m.metadata.labels = m.metadata.labels || envParams.metadata.labels || {};
-  m.metadata.labels.component =
-    process.env.COMPONENT || `restore-${envParams.environment}`;
-});
-
-// override initContainer PGDATABASE/PGPASSWORD because this project pipeline use the legacy `db_SHA` convention instead of `autodevops_SHA`
-const job = manifests.find<Job>((m): m is Job => m.kind === "Job");
-ok(job?.metadata, "Missing job metadata");
-job.metadata.name = `restore-db-${envParams.environment}-${envParams.shortSha}`;
-job.metadata!.annotations = {
-  "kapp.k14s.io/update-strategy": "always-replace",
-};
-job.spec!.template!.metadata!.annotations = {
-  "kapp.k14s.io/deploy-logs": "for-new-or-existing",
-};
-
 export default manifests;
