8db33ce32e924c0db139ff771305d40bc3ca2cad
Douglas DUTEIL <douglasduteil@gmail.com>
2021-08-26 17:26:43 +0200

    build(k8s): migrate to @socialgouv/kosko-charts to v9.3.5

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -1,12 +1,8 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/hasura";
-import { getDeployment } from "@socialgouv/kosko-charts/utils/getDeployment";
 import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
-import { ok } from "assert";
-import environments from "@socialgouv/kosko-charts/environments";
 
-const envParams = environments(process.env);
-const asyncManifests = create("hasura", {
+export default create("hasura", {
   config: {
     image: getHarborImagePath({ name: "cdtn-admin-hasura" }),
     container: {
@@ -24,35 +20,3 @@ const asyncManifests = create("hasura", {
   },
   env,
 });
-
-export default async () => {
-  const manifests = await asyncManifests;
-
-  if (env.env === "dev") {
-    // HACK(douglasduteil): provide one db per env
-    // The CI_ENVIRONMENT_SLUG is the most useful for this
-    const pgSecretRefName = `azure-pg-user-${envParams.environment}`;
-
-    //
-
-    const deployment = getDeployment(
-      manifests as {
-        apiVersion: string;
-        kind: string;
-      }[]
-    );
-    ok(
-      deployment.spec?.template.spec?.containers[0].envFrom![0].secretRef?.name
-    );
-    ok(
-      deployment.spec.template.spec.initContainers![0].envFrom![0].secretRef
-        ?.name
-    );
-    deployment.spec.template.spec.containers[0].envFrom[0].secretRef.name =
-      pgSecretRefName;
-    deployment.spec.template.spec.initContainers![0].envFrom![0].secretRef.name =
-      pgSecretRefName;
-  }
-
-  return manifests;
-};
