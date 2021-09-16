7a72c2afb02f37238cd8c808fb2be7a36cd9aac3
LionelB <lionel@lumographe.fr>
2021-09-16 15:55:41 +0200

    update

--- targets/alert-cli/babel.config.js
+++ targets/alert-cli/babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-modules-commonjs"],
+      presets: ["@babel/preset-typescript"],
+    },
+  },
   presets: [
     [
       "@babel/preset-env",
