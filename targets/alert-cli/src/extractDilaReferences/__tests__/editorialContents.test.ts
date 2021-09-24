9a15d367ce729f0242d491c5b67ebe10619534d6
Lionel <lionel.breduillieard@beta.gouv.fr>
2021-09-24 09:00:57 +0200

    feat(alert): handle alert for information pages (#612)
    * feat(alert): handle alert for information pages
    
    * fix test
    
    * fix lint

---
+++ targets/alert-cli/src/extractDilaReferences/__tests__/editorialContents.test.ts
@@ -0,0 +1,83 @@
+import type { DocumentReference } from "@shared/types";
+
+import type { EditorialContentSubset } from "../editorialContent";
+import { extractEditorialContentTemplateRef } from "../editorialContent";
+import payload from "./mocks/editorialContent.payload.json";
+
+jest.mock("@shared/graphql-client", () => ({
+  __esModule: true, // this property makes it work
+  client: jest.fn(),
+}));
+
+jest.mock("@shared/dila-resolver", () => {
+  const originalModule = jest.requireActual("@shared/dila-resolver");
+  /* eslint-disable-next-line */
+  return {
+    __esModule: true, // this property makes it work
+    ...originalModule,
+    createGetArticleReference: () =>
+      async function (id: string): Promise<DocumentReference> {
+        return Promise.resolve({
+          dila_cid: id,
+          dila_container_id: "cdtn",
+          dila_id: id,
+          title: `article ${id}`,
+          url: "",
+        });
+      },
+  };
+});
+
+test("extractMailTemplateRef", async () => {
+  const references = await extractEditorialContentTemplateRef(
+    payload as EditorialContentSubset[]
+  );
+  expect(references).toMatchInlineSnapshot(`
+Array [
+  Object {
+    "document": Object {
+      "id": "be3ba4c0-4540-4c38-9203-65bc732486a0",
+      "source": "information",
+      "title": "Intéressement et participation : nouveautés Covid-19",
+    },
+    "references": Array [
+      Object {
+        "dila_cid": "LEGIARTI000041747466",
+        "dila_container_id": "cdtn",
+        "dila_id": "LEGIARTI000041747466",
+        "title": "article LEGIARTI000041747466",
+        "url": "",
+      },
+      Object {
+        "dila_cid": "LEGIARTI000041979663",
+        "dila_container_id": "cdtn",
+        "dila_id": "LEGIARTI000041979663",
+        "title": "article LEGIARTI000041979663",
+        "url": "",
+      },
+      Object {
+        "dila_cid": "LEGIARTI000038613233",
+        "dila_container_id": "cdtn",
+        "dila_id": "LEGIARTI000038613233",
+        "title": "article LEGIARTI000038613233",
+        "url": "",
+      },
+      Object {
+        "dila_cid": "LEGIARTI000041973737",
+        "dila_container_id": "cdtn",
+        "dila_id": "LEGIARTI000041973737",
+        "title": "article LEGIARTI000041973737",
+        "url": "",
+      },
+      Object {
+        "dila_cid": "LEGIARTI000042012392",
+        "dila_container_id": "cdtn",
+        "dila_id": "LEGIARTI000042012392",
+        "title": "article LEGIARTI000042012392",
+        "url": "",
+      },
+    ],
+  },
+]
+`);
+});
---
+++ targets/alert-cli/src/extractDilaReferences/editorialContent.ts
@@ -0,0 +1,72 @@
+import {
+  createGetArticleReference,
+  extractArticleId,
+} from "@shared/dila-resolver";
+import type {
+  DocumentReferences,
+  EditoralContentReferenceBloc,
+  EditorialContent,
+} from "@shared/types";
+import { SOURCES } from "@socialgouv/cdtn-sources";
+import { DilaApiClient } from "@socialgouv/dila-api-client";
+import memoizee from "memoizee";
+import pMap from "p-map";
+
+import { getAllDocumentsBySource } from "./getAllDocumentsBySource";
+
+export type EditorialContentSubset = Pick<
+  EditorialContent,
+  "document" | "source" | "title"
+> & {
+  initialId: string;
+  cdtnId: string;
+};
+
+const getArticleReference = createGetArticleReference(new DilaApiClient());
+
+export async function extractEditorialContentTemplateRef(
+  editorialContent: EditorialContentSubset[]
+): Promise<DocumentReferences[]> {
+  const refs: DocumentReferences[] = [];
+
+  for (const docData of editorialContent) {
+    const articleIds: string[] = (docData.document.references ?? [])
+      .flatMap((bloc: EditoralContentReferenceBloc) =>
+        bloc.links.flatMap((link) => extractArticleId(link.url))
+      )
+      .concat(
+        docData.document.contents.flatMap((part) =>
+          part.references.flatMap((bloc) =>
+            bloc.links.flatMap((link) => extractArticleId(link.url))
+          )
+        )
+      );
+    const references = await pMap(
+      articleIds,
+      async (id: string) => getArticleReference(id),
+      { concurrency: 5 }
+    );
+
+    refs.push({
+      document: {
+        id: docData.initialId,
+        source: SOURCES.EDITORIAL_CONTENT,
+        title: docData.title,
+      },
+      references,
+    });
+  }
+  return refs;
+}
+
+async function getEditorialContentTemplateReferences() {
+  const letters = (await getAllDocumentsBySource([
+    SOURCES.EDITORIAL_CONTENT,
+  ])) as EditorialContentSubset[];
+  const documentReferences = await extractEditorialContentTemplateRef(letters);
+  return documentReferences;
+}
+
+export default memoizee(getEditorialContentTemplateReferences, {
+  promise: true,
+});
