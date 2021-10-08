4fd20387d0982142f207f5d0708f60cdd500c3b2
Martial Maillot <martial.maillot@gmail.com>
2021-10-08 18:39:47 +0200

    fix: undefined values on DILA API (#642)
    * fix: undefined values on DILA API
    
    * ci: add logger in dila-resolver jobs dep
    
    * ci: change stage
    
    * revert: remove logger
    
    * ci: fix pre
    
    * fix: lint
    
    Co-authored-by: LionelB <lionel@lumographe.fr>

--- shared/dila-resolver/src/index.ts
+++ shared/dila-resolver/src/index.ts
@@ -5,26 +5,34 @@ import type DilaApiClient from "@socialgouv/dila-api-client";
 import type { ArticlePayload, ArticleVersion } from "./types";
 
 function createGetArticleReference(client: DilaApiClient) {
-  return async function getArticle(id: string): Promise<DocumentReference> {
-    const data = (await client.fetch({
-      method: "POST",
-      params: {
-        id,
-      },
-      path: "/consult/getArticle",
-    })) as ArticlePayload;
-    const [lastVersion] = data.article.articleVersions.slice(-1);
-    const containerId =
-      data.article.conteneurs.length > 0
-        ? data.article.conteneurs[0].cid
-        : data.article.textTitles[0].id;
-    return {
-      dila_cid: data.article.cid,
-      dila_container_id: containerId,
-      dila_id: lastVersion.id,
-      title: `${data.article.nature} ${data.article.num}`,
-      url: "",
-    };
+  return async function getArticle(
+    id: string
+  ): Promise<DocumentReference | null> {
+    try {
+      const apiResult = (await client.fetch({
+        method: "POST",
+        params: {
+          id,
+        },
+        path: "/consult/getArticle",
+      })) as Record<string, unknown>;
+      const data = checkApiResponse(id, apiResult);
+      const [lastVersion] = data.article.articleVersions.slice(-1);
+      const containerId =
+        data.article.conteneurs.length > 0
+          ? data.article.conteneurs[0].cid
+          : data.article.textTitles[0].id;
+      return {
+        dila_cid: data.article.cid,
+        dila_container_id: containerId,
+        dila_id: lastVersion.id,
+        title: `${data.article.nature} ${data.article.num}`,
+        url: "",
+      };
+    } catch (e: unknown) {
+      console.error(`Error retrieving info about article id ${id}`, e);
+      return null;
+    }
   };
 }
 
@@ -42,6 +50,19 @@ function getLatestVersion(articleVersions: ArticleVersion[]): ArticleVersion {
 function extractArticleId(url: string): string[] {
   const match = /(?:LEGI|KALI)ARTI\d{12}/.exec(url);
   return match && match.length > 0 ? match.slice(0, 1) : [];
+}
+
+function checkApiResponse(
+  id: string,
+  data: Record<string, unknown>
+): ArticlePayload {
+  if (Object.keys(data).length === 1) {
+    throw new Error(
+      `invalid response for ${id}, payload: ${JSON.stringify(data)}`
+    );
+  }
+
+  return data as unknown as ArticlePayload;
 }
 
 export { createGetArticleReference, extractArticleId, getLatestVersion };
