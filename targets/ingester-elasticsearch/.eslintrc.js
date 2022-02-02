08b982a027752e4e55bd09df48d591af5c5fc5df
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-02-02 17:22:32 +0100

    fix: linter all project

---
+++ targets/ingester-elasticsearch/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: "@socialgouv/eslint-config-typescript",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "@typescript-eslint/member-ordering": "warn",
+  },
+};
