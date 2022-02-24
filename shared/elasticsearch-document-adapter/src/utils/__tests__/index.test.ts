27cc3e8742294c68201eba9f26e835c01db0a827
Martial Maillot <martial.maillot@gmail.com>
2022-02-24 11:46:46 +0100

    review feedback

--- shared/elasticsearch-document-adapter/src/utils/__tests__/index.test.ts
+++ shared/elasticsearch-document-adapter/src/utils/__tests__/index.test.ts
@@ -2,33 +2,35 @@ import { isHTML, keyFunctionParser } from "..";
 
 describe("keyFunctionParser", () => {
   test.each`
-    key           | object  | fn                | expected
-    ${"markdown"} | ${null} | ${() => "result"} | ${null}
+    key           | object  | expected
+    ${"markdown"} | ${null} | ${null}
     ${"markdown"} | ${{
   markdown: "wesh",
   res: { markdown: "ok", rien: "yo", yo: { markdown: "yes" } },
-}} | ${() => "result"} | ${{
+}} | ${{
   markdown: "result",
   res: { markdown: "result", rien: "yo", yo: { markdown: "result" } },
 }}
     ${"markdown"} | ${{
   markdown: "wesh",
   res: { markdown: "ok", resp: [{ markdown: "no", sol: "oui" }], rien: "yo", yo: { markdown: "yes" } },
-}} | ${() => "result"} | ${{
+}} | ${{
   markdown: "result",
   res: { markdown: "result", resp: [{ markdown: "result", sol: "oui" }], rien: "yo", yo: { markdown: "result" } },
 }}
     ${"markdown"} | ${[{
     markdown: "wesh",
     res: { markdown: "ok", resp: [{ markdown: "no", sol: "oui" }], rien: "yo", yo: { markdown: "yes" } },
-  }]} | ${() => "result"} | ${[{
+  }]} | ${[{
     markdown: "result",
     res: { markdown: "result", resp: [{ markdown: "result", sol: "oui" }], rien: "yo", yo: { markdown: "result" } },
   }]}
   `(
     "should return $expected for $object for this $key",
-    ({ key, object, fn, expected }) => {
-      expect(keyFunctionParser(key, object, fn)).toStrictEqual(expected);
+    ({ key, object, expected }) => {
+      expect(keyFunctionParser(key, object, () => "result")).toStrictEqual(
+        expected
+      );
     }
   );
 });
