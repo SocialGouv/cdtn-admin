340acb3288ec45056bc22e085bf3d879989d7066
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-14 16:03:30 +0100

    fix: build

--- .k8s/components/ingester.cronjob.ts
+++ .k8s/components/ingester.cronjob.ts
@@ -18,10 +18,6 @@ const labels = merge(envParams.metadata.labels || {}, {
   app: name,
 });
 
-const tag = process.env.CI_COMMIT_TAG
-  ? process.env.CI_COMMIT_TAG.slice(1)
-  : process.env.CI_COMMIT_SHA;
-
 const persistentVolumeClaim = new PersistentVolumeClaim({
   metadata: {
     annotations,
