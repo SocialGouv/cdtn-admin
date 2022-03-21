c7a66e20938e31ed0fb60a984e8fafbb12b02b3b
Martial Maillot <martial.maillot@gmail.com>
2022-03-21 11:34:44 +0100

    ci: fix backoffLimit value

--- .k8s/components/alert.cronjob.ts
+++ .k8s/components/alert.cronjob.ts
@@ -55,7 +55,7 @@ export default async () => {
       schedule: "0 1 * * *",
       jobTemplate: {
         spec: {
-          backoffLimit: 0,
+          backoffLimit: 1,
           template: {
             metadata: {
               annotations: merge(annotations, {
