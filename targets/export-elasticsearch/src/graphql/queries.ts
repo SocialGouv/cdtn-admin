8d797a6c57028bc802496c5cb98e3cfbacdb36ee
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-21 20:01:41 +0100

    feat: add query

--- targets/export-elasticsearch/src/graphql/queries.ts
+++ targets/export-elasticsearch/src/graphql/queries.ts
@@ -1,1 +1,35 @@
-export const getStatusPipeline = ``;
+export const getExportEsStatusById = `
+query getExportEsStatusById($id: uuid!) {
+  export_es_status_by_pk(id: $id) {
+    id
+    environment
+    status
+    user_id
+    created_at
+    updated_at
+  }
+}`;
+
+export const getExportEsStatusByEnvironments = `
+query getExportEsStatusByEnvironments($environment: String!) {
+  export_es_status(where: {environment: {_eq: $environment}}) {
+    id
+    environment
+    status
+    user_id
+    created_at
+    updated_at
+  }
+}`;
+
+export const getExportEsStatusByStatus = `
+query getExportEsStatusByStatus($status: String!) {
+  export_es_status(where: {status: {_eq: $status}}) {
+    id
+    environment
+    status
+    user_id
+    created_at
+    updated_at
+  }
+}`;
---
+++ targets/hasura/migrations/1647885999287_create_table_public_export_es_status/down.sql
@@ -0,0 +1,1 @@
+DROP TABLE "public"."export_es_status";
