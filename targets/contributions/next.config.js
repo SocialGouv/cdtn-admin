11381f23b30f341b3c665ad6be68c2edf5c45cb1
Martial Maillot <martial.maillot@gmail.com>
2022-08-31 09:43:30 +0200

    feat(headers): add security headers to frontend and contributions (#802)

--- targets/contributions/next.config.js
+++ targets/contributions/next.config.js
@@ -5,4 +5,20 @@ module.exports = {
       process.env.CDTN_API_URL || "https://cdtn-api.fabrique.social.gouv.fr",
     API_URL: process.env.API_URL || "http://localhost:8080",
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "deny"
+          },
+          { key: "X-XSS-Protection", value: "1; mode=block" },
+          { key: "X-Content-Type-Options", value: "nosniff" }
+        ],
+      },
+    ];
+  },
 };
