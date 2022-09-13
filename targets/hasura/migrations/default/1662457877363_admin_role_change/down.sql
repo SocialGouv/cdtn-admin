ee5af43b491cbac1a6634dd843e468bf0480ecc4
Victor Zeinstra <zeinsta.victor@gmail.com>
2022-09-13 11:51:45 +0200

    fix: migration script fix

--- targets/hasura/migrations/default/1662457877363_admin_role_change/down.sql
+++ targets/hasura/migrations/default/1662457877363_admin_role_change/down.sql
@@ -1,11 +1,4 @@
-ALTER TABLE public.roles DISABLE TRIGGER ALL;
-update public.roles set role='admin' where role = 'super';
-ALTER TABLE public.roles ENABLE TRIGGER ALL;
-
-ALTER TABLE auth.user_roles DISABLE TRIGGER ALL;
+insert into public.roles(role) values ('admin');
 update auth.user_roles set role = 'admin' where role = 'super';
-ALTER TABLE auth.user_roles ENABLE TRIGGER ALL;
-
-ALTER TABLE auth.users DISABLE TRIGGER ALL;
 update auth.users set default_role = 'admin' where default_role = 'super';
-ALTER TABLE auth.users ENABLE TRIGGER ALL;
+delete from public.roles where role='super';
--- targets/hasura/migrations/default/1662457877363_admin_role_change/up.sql
+++ targets/hasura/migrations/default/1662457877363_admin_role_change/up.sql
@@ -1,11 +1,4 @@
-ALTER TABLE public.roles DISABLE TRIGGER ALL;
-update public.roles set role='super' where role = 'admin';
-ALTER TABLE public.roles ENABLE TRIGGER ALL;
-
-ALTER TABLE auth.user_roles DISABLE TRIGGER ALL;
+insert into public.roles(role) values ('super');
 update auth.user_roles set role = 'super' where role = 'admin';
-ALTER TABLE auth.user_roles ENABLE TRIGGER ALL;
-
-ALTER TABLE auth.users DISABLE TRIGGER ALL;
 update auth.users set default_role = 'super' where default_role = 'admin';
-ALTER TABLE auth.users ENABLE TRIGGER ALL;
+delete from public.roles where role='admin';
