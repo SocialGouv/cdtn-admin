b4bb2b3025b94174b9e55a647137fea370d2a680
Victor Zeinstra <zeinstra.victor@gmail.com>
2022-09-21 17:49:13 +0200

    fix: contrib edition (#816)
    * fix: contrib edition
    
    * refactor
    
    * refactor
    
    Co-authored-by: Victor Zeinstra <zeinsta.victor@gmail.com>

--- targets/frontend/src/pages/contenus/[id].tsx
+++ targets/frontend/src/pages/contenus/[id].tsx
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Button } from "src/components/button";
 import { Layout } from "src/components/layout/auth.layout";
@@ -10,7 +10,6 @@ import { Stack } from "src/components/layout/Stack";
 import { withCustomUrqlClient } from "src/hoc/CustomUrqlClient";
 import { withUserProvider } from "src/hoc/UserProvider";
 import { previewContentAction } from "src/lib/preview/preview.gql";
-import { ContentUpdateMutation } from "src/types";
 import { Card, Message, NavLink } from "theme-ui";
 import { useMutation, useQuery } from "urql";
 
@@ -30,77 +29,54 @@ export function DocumentPage() {
       id: router.query.id,
     },
   });
-  const { fetching, error, data } = result;
+  const { fetching, error, data: dataDocument } = result;
 
   const [hasChanged, setHasChanged] = useState(false);
-  const [submitIdle, setSubmitIdle] = useState(false);
+  const [jsonData, setJsonData] = useState(
+    JSON.stringify(dataDocument?.document, undefined, 2)
+  );
   const [, executeUpdate] = useMutation(updateDocumentMutation);
   const [, previewContent] = useMutation(previewContentAction);
   const { handleSubmit } = useForm();
 
-  const jsonDoc = useRef(null);
-  if (!fetching && !error && jsonDoc.current === null) {
-    jsonDoc.current = data.document;
-  }
-
-  const current = jsonDoc.current as ContentUpdateMutation | null;
-
-  function onEditSubmit() {
-    setSubmitIdle(true);
-    return executeUpdate({
-      cdtnId: data.document.cdtn_id,
-      document: current?.document,
-      isAvailable: data.document.is_available,
+  async function onEditSubmit() {
+    const current = JSON.parse(jsonData);
+    const { data, error: errorUpdate }: any = await executeUpdate({
+      cdtnId: router.query.id,
+      document: current.document,
+      isAvailable: current.is_available,
       metaDescription: current?.meta_description,
       text: current?.text,
       title: current?.title,
-    }).then(({ data, error }) => {
-      if (!data) {
-        console.error("update impossible");
-        return;
-      }
-      const { cdtnId, source, document, metaDescription, text, title } =
-        data?.document;
-      if (error) {
-        console.error("update impossible", error.message);
-      }
-      previewContent({
-        cdtnId,
-        document: {
-          ...document,
-          metaDescription,
-          text,
-          title,
-        },
-        source,
-      }).then((response) => {
-        if (response.error) {
-          console.error("preview impossible", response.error.message);
-        }
-      });
-      setHasChanged(false);
-      setSubmitIdle(false);
     });
+    setHasChanged(false);
+    if (!data) {
+      console.error("update impossible");
+      return;
+    }
+    const { cdtnId, source, document, metaDescription, text, title } =
+      data?.document;
+    if (errorUpdate) {
+      console.error("update impossible", errorUpdate.message);
+    }
+    const response: any = previewContent({
+      cdtnId,
+      document: {
+        ...document,
+        metaDescription,
+        text,
+        title,
+      },
+      source,
+    });
+    if (response.error) {
+      console.error("preview impossible", response.error.message);
+    }
   }
 
   function handleEditorChange(stringifyCode: string) {
-    try {
-      const newJson = JSON.parse(stringifyCode);
-      if (
-        JSON.stringify(newJson, undefined, 2) !==
-        JSON.stringify(data.document, undefined, 2)
-      ) {
-        jsonDoc.current = newJson;
-        setHasChanged(true);
-      } else if (
-        stringifyCode === JSON.stringify(data.document, undefined, 2)
-      ) {
-        jsonDoc.current = data.document;
-        setHasChanged(false);
-      }
-    } catch (err) {
-      return;
-    }
+    setJsonData(stringifyCode);
+    setHasChanged(true);
   }
 
   if (fetching) {
@@ -111,21 +87,21 @@ export function DocumentPage() {
     return <Message variant="primary">{error.message}</Message>;
   }
   return (
-    <Layout title={data.document.title}>
+    <Layout title={dataDocument.document.title}>
       <form onSubmit={handleSubmit(onEditSubmit)}>
         <Stack>
           <Card>
             {/*
               // @ts-ignore */}
             <CodeWithCodemirror
-              value={JSON.stringify(jsonDoc.current, undefined, 2)}
+              value={jsonData}
               onChange={handleEditorChange}
             />
           </Card>
           <Inline>
             {/*
               // @ts-ignore */}
-            <Button disabled={submitIdle || !hasChanged}>Enregistrer</Button>
+            <Button disabled={!hasChanged}>Enregistrer</Button>
             <Link href="/contenus" passHref>
               <NavLink
                 onClick={(e) => {
