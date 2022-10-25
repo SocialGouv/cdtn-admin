60fc7e4b1763cb324495966f9bfedb87004eae7f
Victor Zeinstra <zeinstra.victor@gmail.com>
2022-10-25 09:37:28 +0200

    chore: fix tsx tests for admin frontend (#832)
    Co-authored-by: Victor Zeinstra <zeinsta.victor@gmail.com>

---
+++ targets/frontend/src/__tests__/contenus/contentList.test.tsx
@@ -0,0 +1,24 @@
+import { render, RenderResult } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { DocumentsPage } from "src/pages/contenus";
+
+jest.mock("next/router", () => ({
+  ...jest.requireActual("next/router"),
+  useRouter: () => ({
+    query: {},
+  }),
+}));
+
+describe("Given parameters", () => {
+  describe("When rendering the component DocumentsPage", () => {
+    let rendering: RenderResult;
+    beforeEach(async () => {
+      await act(async () => {
+        rendering = render(<DocumentsPage />);
+      });
+    });
+    it("doit afficher le titre", () => {
+      expect(rendering.queryByText("Contenus")).toBeInTheDocument();
+    });
+  });
+});
