f0bda49e06bb75dc7bb480986c8b47bae7842361
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-05-11 16:43:09 +0200

    fix: add migration

---
+++ targets/hasura/migrations/default/1683815452188_create_table_contribution_answer_comments/up.sql
@@ -0,0 +1,2 @@
+CREATE TABLE "contribution"."answer_comments" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "content" text NOT NULL, "answer_id" uuid NOT NULL, "user_id" uuid, "created_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") , FOREIGN KEY ("answer_id") REFERENCES "contribution"."answers"("id") ON UPDATE cascade ON DELETE cascade, FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE set null ON DELETE set null, UNIQUE ("id"));
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
