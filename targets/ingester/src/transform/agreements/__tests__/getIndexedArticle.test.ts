bba23ce45fdcea5a1cb6f1379490e573dd78de30
carolineBda <caroline.bourdeu@gmail.com>
2023-05-15 16:44:45 +0200

    reformat

--- targets/ingester/src/transform/agreements/__tests__/getIndexedArticle.test.ts
+++ targets/ingester/src/transform/agreements/__tests__/getIndexedArticle.test.ts
@@ -3,40 +3,33 @@ import { readFileSync } from "fs";
 import { loadArticles } from "../data-loaders";
 import getIndexedArticle from "../getIndexedArticle";
 
-describe(`libs/getIndexedArticle()`, () => {
-  describe(`should match properties`, () => {
-    const data = readFileSync(
-      "node_modules/@socialgouv/kali-data/data/articles/index.json"
-    ).toString();
-    const articles = JSON.parse(data);
-    it(`with an existing article ID`, () => {
-      expect(getIndexedArticle(articles, "KALIARTI000023306963")).toMatchObject(
-        {
-          agreementId: "KALICONT000005635085",
-          articleCid: "KALIARTI000005768420",
-          articleId: "KALIARTI000023306963",
-        }
-      );
-    });
+describe(`libs/getIndexedArticle() should match properties`, () => {
+  const data = readFileSync(
+    "node_modules/@socialgouv/kali-data/data/articles/index.json"
+  ).toString();
+  const articles = JSON.parse(data);
 
-    it(`with an existing article CID`, () => {
-      expect(getIndexedArticle(articles, "KALIARTI000005768420")).toMatchObject(
-        {
-          agreementId: "KALICONT000005635085",
-          articleCid: "KALIARTI000005768420",
-          articleId: "KALIARTI000023306963",
-        }
-      );
+  it(`with an existing article ID`, () => {
+    expect(getIndexedArticle(articles, "KALIARTI000023306963")).toMatchObject({
+      agreementId: "KALICONT000005635085",
+      articleCid: "KALIARTI000005768420",
+      articleId: "KALIARTI000023306963",
     });
   });
 
-  describe(`should return undefined`, () => {
-    it(`with a nonexistent article ID or CID`, async () => {
-      const articles = await loadArticles();
-
-      expect(
-        getIndexedArticle(articles, "KALIARTI123456789012")
-      ).toBeUndefined();
+  it(`with an existing article CID`, () => {
+    expect(getIndexedArticle(articles, "KALIARTI000005768420")).toMatchObject({
+      agreementId: "KALICONT000005635085",
+      articleCid: "KALIARTI000005768420",
+      articleId: "KALIARTI000023306963",
     });
+  });
+});
+
+describe(`should return undefined`, () => {
+  it(`with a nonexistent article ID or CID`, async () => {
+    const articles = await loadArticles();
+
+    expect(getIndexedArticle(articles, "KALIARTI123456789012")).toBeUndefined();
   });
 });
