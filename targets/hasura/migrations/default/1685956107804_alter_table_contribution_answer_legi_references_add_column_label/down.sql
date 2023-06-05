538c2da3aba414bf3b5a03f748e1844402abb300
carolineBda <caroline.bourdeu@gmail.com>
2023-06-05 11:10:37 +0200

    feat(contributions): mise à jour des tables de références legi et kali pour ajouter une colonne label

---
+++ targets/hasura/migrations/default/1685956107804_alter_table_contribution_answer_legi_references_add_column_label/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "contribution"."answer_legi_references" add column "label" text
+--  null;
---
+++ targets/hasura/migrations/default/1685956107804_alter_table_contribution_answer_legi_references_add_column_label/up.sql
@@ -0,0 +1,2 @@
+alter table "contribution"."answer_legi_references" add column "label" text
+ null;
