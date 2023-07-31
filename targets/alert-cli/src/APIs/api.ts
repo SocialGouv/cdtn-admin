e47f62f70481195fb969a0e5645ea3804df6c9ff
Martial Maillot <martial.maillot@gmail.com>
2023-07-31 16:12:44 +0200

    fix: on new file, patch return old file with value /dev/null (#983)

--- targets/alert-cli/src/APIs/api.ts
+++ targets/alert-cli/src/APIs/api.ts
@@ -79,7 +79,7 @@ export class GithubApi {
 
   async raw(project: string, path: string, tag: GitTagData): Promise<string> {
     const url = `https://raw.githubusercontent.com/${project}/${tag.ref}/${path}`;
-    const data = await this.fetchText(url, true);
+    const data = await this.fetchText(url);
     return data;
   }
 
@@ -103,19 +103,12 @@ export class GithubApi {
     return data as T;
   }
 
-  private async fetchText(url: string, ignoreError?: boolean): Promise<string> {
+  private async fetchText(url: string): Promise<string> {
     const response = await this.fetchGithub(url);
     if (!response.ok) {
-      if (ignoreError) {
-        console.error(
-          `Failed to fetch ${url} - ${response.status} : ${response.statusText}`
-        );
-        return "";
-      } else {
-        throw new Error(
-          `Failed to fetch ${url} - ${response.status} : ${response.statusText}`
-        );
-      }
+      throw new Error(
+        `Failed to fetch ${url} - ${response.status} : ${response.statusText}`
+      );
     }
     return response.text();
   }
