a2c1192c213205ecad069f4cf743b3c21d60d856
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-09-19 16:05:18 +0200

    fix: recherche par slug

--- targets/frontend/src/components/contributions/answers/references/cdtnReferencesSearch.query.ts
+++ targets/frontend/src/components/contributions/answers/references/cdtnReferencesSearch.query.ts
@@ -3,28 +3,6 @@ import { useQuery } from "urql";
 import { CdtnReference, Document } from "../../type";
 import { Result } from "./ReferenceInput";
 
-export const contributionSearchCdtnReferencesSearch = `
-query SearchCdtnReferences($query: String, $sources: [String!]) {
-  documents(where: {
-    _or: [
-      {slug: {_ilike: $query}},
-      {title: {_ilike: $query}}
-    ],
-    is_available: {_eq: true},
-    is_published: {_eq: true},
-    source: {_in: $sources}
-  },
-  order_by: {slug: asc},
-    limit: 10
-    ) {
-    title
-    cdtnId: cdtn_id
-    source
-    slug
-  }
-}
-`;
-
 type QueryResult = {
   documents: Document[];
 };
@@ -32,29 +10,12 @@ type QueryResult = {
 export const useContributionSearchCdtnReferencesQuery = (
   query: string | undefined
 ): Result<Pick<CdtnReference, "document">> => {
-  const querySplit = query?.split(/[ -,]/);
-  const slugQuery = querySplit?.map(
-    (text) =>
-      `{slug: {_ilike: "%${text
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")}%"}}`
-  ).join(`,
-  `);
-  const titleQuery = querySplit?.map(
-    (text) =>
-      `{title: {_ilike: "%${text
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")}%"}}`
-  ).join(`,
-  `);
+  const [slug] = query?.split("/").reverse() ?? [""];
   const [{ data, fetching, error }] = useQuery<QueryResult>({
     query: `
-    query SearchCdtnReferences($sources: [String!]) {
+    query SearchCdtnReferences($sources: [String!], $slug: String) {
       documents(where: {
-        _or: [
-          {_and: [${slugQuery}]}
-          {_and: [${titleQuery}]}
-        ],
+        slug: {_eq: $slug},
         is_available: {_eq: true},
         is_published: {_eq: true},
         source: {_in: $sources}},
@@ -78,6 +39,7 @@ export const useContributionSearchCdtnReferencesQuery = (
         "contributions",
         "fiches_service_public",
       ],
+      slug,
     },
   });
   return {
