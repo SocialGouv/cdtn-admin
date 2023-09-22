97892ce96cabf021f6db8da497de0afb8b895a93
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-09-22 17:00:36 +0200

    fix: test

--- targets/frontend/src/components/contributions/questionList/__tests__/QuestionList.test.tsx
+++ targets/frontend/src/components/contributions/questionList/__tests__/QuestionList.test.tsx
@@ -13,7 +13,7 @@ describe("QuestionList", () => {
     expect(screen.getByTestId("contributions-list-search")).toBeInTheDocument();
   });
   test("Verify question display", () => {
-    expect(screen.queryByText("question1")).toBeInTheDocument();
-    expect(screen.queryByText("question2")).toBeInTheDocument();
+    expect(screen.getByText(/question1/)).toBeInTheDocument();
+    expect(screen.getByText(/question2/)).toBeInTheDocument();
   });
 });
