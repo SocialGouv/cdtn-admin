75616c6d550f491006109217245b02662ac28f85
devthejo <devthejo@protonmail.com>
2023-09-25 23:38:35 +0200

    fix: jest

--- targets/ingester/jest.config.js
+++ targets/ingester/jest.config.js
@@ -3,8 +3,8 @@ const config = {
   coverageDirectory: "coverage",
   preset: "ts-jest/presets/js-with-ts-esm",
   collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
   roots: ["<rootDir>/src"],
   testEnvironment: "node",
@@ -13,6 +13,7 @@ const config = {
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
+  transformIgnorePatterns: ["<rootDir>/node_modules/(?!(strip-markdown)/)"],
 };
 
 module.exports = config;
