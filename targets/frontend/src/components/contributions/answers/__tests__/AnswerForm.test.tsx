fd0ecabd092b4e90926dadf6bb7ecc781fdcc9d6
Victor Zeinstra <zeinsta.victor@gmail.com>
2023-10-05 17:34:20 +0200

    chore: add TU

--- targets/frontend/src/components/contributions/answers/__tests__/AnswerForm.test.tsx
+++ targets/frontend/src/components/contributions/answers/__tests__/AnswerForm.test.tsx
@@ -3,56 +3,81 @@ import userEvent from "@testing-library/user-event";
 import React from "react";
 
 import { AnswerForm } from "../AnswerForm";
+import { AnswerWithStatus } from "../answer.query";
 
-describe("Given a component AnswerForm and a basic default non generic answer", () => {
+const answerBase: AnswerWithStatus = {
+  id: "369336d2-994f-48b1-b6ac-fec78cff240e",
+  questionId: "2c820037-62bd-4c0e-a1a8-ca80b97b5958",
+  agreementId: "0000",
+  content: "",
+  contentType: "ANSWER",
+  updatedAt: "2023-09-29T14:09:52.01401+00:00",
+  contentServicePublicCdtnId: null,
+  question: {
+    id: "2c820037-62bd-4c0e-a1a8-ca80b97b5958",
+    content: "Quelle est la durée maximale de la période d’essai ?",
+    order: 5,
+  },
+  agreement: {
+    id: "0000",
+    name: "Convention collective nationale des transports routiers et activités auxiliaires du transport",
+    kaliId: "KALICONT000005635624",
+  },
+  answerComments: [],
+  statuses: [],
+  kaliReferences: [],
+  legiReferences: [],
+  otherReferences: [],
+  cdtnReferences: [],
+  contentFichesSpDocument: null,
+  status: {
+    status: "TODO",
+  },
+  updateDate: "29/09/2023",
+};
+
+describe("Given a component AnswerForm and a basic default generic answer", () => {
   beforeEach(() => {
-    render(
-      <AnswerForm
-        answer={{
-          id: "369336d2-994f-48b1-b6ac-fec78cff240e",
-          questionId: "2c820037-62bd-4c0e-a1a8-ca80b97b5958",
-          agreementId: "0016",
-          content: "",
-          contentType: "ANSWER",
-          updatedAt: "2023-09-29T14:09:52.01401+00:00",
-          contentServicePublicCdtnId: null,
-          question: {
-            id: "2c820037-62bd-4c0e-a1a8-ca80b97b5958",
-            content: "Quelle est la durée maximale de la période d’essai ?",
-            order: 5,
-          },
-          agreement: {
-            id: "0016",
-            name: "Convention collective nationale des transports routiers et activités auxiliaires du transport",
-            kaliId: "KALICONT000005635624",
-          },
-          answerComments: [],
-          statuses: [],
-          kaliReferences: [],
-          legiReferences: [],
-          otherReferences: [],
-          cdtnReferences: [],
-          contentFichesSpDocument: null,
-          status: {
-            status: "TODO",
-          },
-          updateDate: "29/09/2023",
-        }}
-        onSubmit={() => {}}
-      />
-    );
+    render(<AnswerForm answer={answerBase} onSubmit={() => {}} />);
   });
-  describe("When i click on submit", () => {
-    beforeEach(() => {
-      expect(screen.getByText("Afficher la réponse")).toBeInTheDocument();
-      userEvent.click(screen.getByText("Afficher la réponse"));
-      expect(screen.getByText("Soumettre")).toBeInTheDocument();
-      userEvent.click(screen.getByText("Soumettre"));
-    });
-    test("Check if content required error message displays", () => {
-      expect(
-        screen.getByText("Une réponse doit être renseigner")
-      ).toBeInTheDocument();
-    });
+  test("Check options are displayed", () => {
+    expect(screen.queryByText("Afficher la réponse")).toBeInTheDocument();
+    expect(
+      screen.queryByText("Utiliser la fiche service public")
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText("La convention collective ne prévoit rien")
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("Nous n'avons pas la réponse")
+    ).not.toBeInTheDocument();
+  });
+});
+
+describe("Given a component AnswerForm and a basic default CC answer", () => {
+  beforeEach(() => {
+    const answer = {
+      ...answerBase,
+      agreementId: "0016",
+      agreement: {
+        ...answerBase.agreement,
+        id: "0016",
+        name: "0016",
+        kaliId: "0016",
+      },
+    };
+    render(<AnswerForm answer={answer} onSubmit={() => {}} />);
+  });
+  test("Check options are displayed", () => {
+    expect(screen.queryByText("Afficher la réponse")).toBeInTheDocument();
+    expect(
+      screen.queryByText("Utiliser la fiche service public")
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("La convention collective ne prévoit rien")
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText("Nous n'avons pas la réponse")
+    ).toBeInTheDocument();
   });
 });
