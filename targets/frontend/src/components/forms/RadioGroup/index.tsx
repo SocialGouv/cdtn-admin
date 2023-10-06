2a0ace55918f3bda72a34ae9ce8b81b92fdd5a16
Caroline <4971715+carolineBda@users.noreply.github.com>
2023-10-06 11:06:28 +0200

    fix(wording): rename answer type on contribution page (#1052)

--- targets/frontend/src/components/forms/RadioGroup/index.tsx
+++ targets/frontend/src/components/forms/RadioGroup/index.tsx
@@ -1,13 +1,13 @@
 import {
   FormControl,
   FormControlLabel,
-  FormLabel,
   Radio,
   RadioGroup,
 } from "@mui/material";
 import React, { PropsWithChildren } from "react";
 import { Controller } from "react-hook-form";
 import { CommonFormProps } from "../type";
+import { TitleBox } from "../TitleBox";
 
 type OptionProps = {
   label: string;
@@ -34,10 +34,9 @@ export const FormRadioGroup = ({
       name={name}
       control={control}
       rules={rules}
-      render={({ field: { onChange, value }, fieldState: { error } }) => {
-        return (
-          <FormControl fullWidth={fullWidth} error={!!error}>
-            <FormLabel>{label}</FormLabel>
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <FormControl fullWidth={fullWidth} error={!!error}>
+          <TitleBox title={label} disabled={disabled}>
             <RadioGroup
               value={value}
               onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
@@ -54,9 +53,9 @@ export const FormRadioGroup = ({
                 />
               ))}
             </RadioGroup>
-          </FormControl>
-        );
-      }}
+          </TitleBox>
+        </FormControl>
+      )}
     />
   );
 };
