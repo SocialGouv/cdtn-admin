9223e626dbd0d006708172951bd67bc22a4653a9
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-10-13 11:08:03 +0200

    fix: types

---
+++ shared/utils/build/src/url-generator.d.ts
@@ -0,0 +1,4 @@
+export declare const generateKaliRef: (kaliArticleId: string, agreementId: string) => string;
+export declare const generateLegiRef: (legiArticleTitle: string, withPrefix?: boolean) => string;
+export declare const generateFichesSpRef: (audience: "associations" | "particuliers" | "professionnels-entreprises", ficheSpInitialId: string) => string;
+//# sourceMappingURL=url-generator.d.ts.map
---
+++ shared/utils/build/src/url-generator.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateFichesSpRef = exports.generateLegiRef = exports.generateKaliRef = void 0;
+const cdtn_utils_1 = require("@socialgouv/cdtn-utils");
+const generateKaliRef = (kaliArticleId, agreementId) => {
+    return `https://legifrance.gouv.fr/conv_coll/id/${kaliArticleId}/?idConteneur=${agreementId}`;
+};
+exports.generateKaliRef = generateKaliRef;
+const generateLegiRef = (legiArticleTitle, withPrefix = true) => {
+    return `${withPrefix ? "https://code.travail.gouv.fr" : ""}/code-du-travail/${(0, cdtn_utils_1.slugify)(legiArticleTitle)}`;
+};
+exports.generateLegiRef = generateLegiRef;
+const generateFichesSpRef = (audience, ficheSpInitialId) => {
+    return `https://www.service-public.fr/${audience}/vosdroits/${ficheSpInitialId}`;
+};
+exports.generateFichesSpRef = generateFichesSpRef;
+//# sourceMappingURL=url-generator.js.map
