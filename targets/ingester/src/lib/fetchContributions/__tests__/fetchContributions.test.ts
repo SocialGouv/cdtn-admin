f6defc43441ee9bbc2911bac2f6cb95131acda27
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-10-13 11:28:33 +0200

    fix: expected by snapshot

--- targets/ingester/src/lib/fetchContributions/__tests__/fetchContributions.test.ts
+++ targets/ingester/src/lib/fetchContributions/__tests__/fetchContributions.test.ts
@@ -1,12 +1,11 @@
 jest.mock("../ContributionRepository");
 jest.mock("../AgreementRepository");
 import { fetchContributions, fetchFicheSPIdsFromContributions } from "../index";
-import ExpectedOutput from "./expected.json";
 
 describe("ContributionRepository", () => {
   test("fetchContribution should return all contributions with the expected format", async () => {
     const result = await fetchContributions();
-    expect(result).toEqual(ExpectedOutput);
+    expect(result).toMatchSnapshot();
   });
 
   test("fetchFicheSPIdsFromContributions should return all fiche SP ids from Published contributions", async () => {
