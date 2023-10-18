ab462540d08896e547e77b56b127f5c2296aca27
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-10-18 17:42:09 +0200

    fix: merge

--- shared/utils/build/src/url-generator.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const generateKaliRef: (kaliArticleId: string, agreementId: string) => string;
-export declare const generateLegiRef: (legiArticleTitle: string, withPrefix?: boolean) => string;
-export declare const generateFichesSpRef: (audience: "associations" | "particuliers" | "professionnels-entreprises", ficheSpInitialId: string) => string;
-export declare const generateFichesSpRefLocal: (urlSlug: string, withPrefix?: boolean) => string;
-//# sourceMappingURL=url-generator.d.ts.map
