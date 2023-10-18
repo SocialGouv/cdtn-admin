ab30e1293dd4c04c3a0cd668cbf243ada657b833
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-10-18 17:30:27 +0200

    fix: test

--- targets/ingester/src/lib/fetchContributions/__tests__/fetchContributions.test.ts
+++ targets/ingester/src/lib/fetchContributions/__tests__/fetchContributions.test.ts
@@ -1,11 +1,12 @@
 jest.mock("../ContributionRepository");
 jest.mock("../AgreementRepository");
 import { fetchContributions, fetchFicheSPIdsFromContributions } from "../index";
+import ExpectedOutput from "./expected.json";
 
 describe("ContributionRepository", () => {
   test("fetchContribution should return all contributions with the expected format", async () => {
     const result = await fetchContributions();
-    expect(result).toMatchSnapshot();
+    expect(result).toEqual(ExpectedOutput);
   });
 
   test("fetchFicheSPIdsFromContributions should return all fiche SP ids from Published contributions", async () => {
