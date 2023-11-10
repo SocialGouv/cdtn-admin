e1c84c76bfb61ffed4ec8f5ebd042e721a6bb52c
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-11-10 18:35:40 +0100

    fix: remove migration

--- targets/hasura/migrations/default/1699626945734_change_document_initial_id_from_contribution/up.sql
+++
@@ -1,7 +0,0 @@
-update documents
-set initial_id = a.id
-from contribution.answers a
-    inner join contribution.questions q on q.id = a.question_id
-where documents."source" = 'contributions'
-    and a.agreement_id = documents."document"->'answers'->'conventionAnswer'->>'idcc'
-    and q."content" = documents."title";
