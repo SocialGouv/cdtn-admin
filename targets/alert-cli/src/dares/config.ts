e9fa5a9075a4eb0b4ef07d842fa271df62fc2c4b
Martial Maillot <martial.maillot@gmail.com>
2023-11-20 18:04:04 +0100

    fix(alert): scrap MT qui est à jour par rapport à CDTN (#1127)

--- targets/alert-cli/src/dares/config.ts
+++ targets/alert-cli/src/dares/config.ts
@@ -1,4 +1,5 @@
 export const URL_SCRAPING =
-  "https://code.travail.gouv.fr/fiche-ministere-travail/conventions-collectives-nomenclatures";
+  "https://travail-emploi.gouv.fr/dialogue-social/negociation-collective/article/conventions-collectives-nomenclatures";
+
 export const URL_KALI =
   "https://raw.githubusercontent.com/SocialGouv/kali-data/master/data/index.json";
