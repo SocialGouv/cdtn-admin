7a5c3cb982961f2d5db5cae5c3c3617586cbbd94
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-11-30 15:50:00 +0100

    feat: add tu

--- shared/elasticsearch-document-adapter/src/agreements/getInfoMessage.ts
+++ shared/elasticsearch-document-adapter/src/agreements/getInfoMessage.ts
@@ -6,13 +6,13 @@ const CC_RENVOIE_CDT =
   "Les informations ci-dessous sont issues du code du travail car la convention collective renvoie au code du travail";
 const CC_MOINS_FAVORABLE =
   "Les informations ci-dessous sont issues du code du travail car elles sont plus favorables que les dispositions prévues par la convention collective";
-const CC_ANSWER =
+const CC_AVEC_REPONSE =
   "Les informations ci-dessous sont issues de l’analyse des règles prévues par votre convention collective de branche étendue et par le Code du travail";
 
 export function getInfoMessage(data: ContributionElasticDocument): string {
   switch (true) {
     case data.contentType === "ANSWER":
-      return CC_ANSWER;
+      return CC_AVEC_REPONSE;
     case data.contentType === "CDT":
       return CC_RENVOIE_CDT;
     case data.contentType === "UNFAVOURABLE":
