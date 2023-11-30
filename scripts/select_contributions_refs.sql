4f32dfbc10b91f0687663d7450a671503c5c6846
Victor <victor.zeinstra@gmail.com>
2023-11-30 11:12:55 +0100

    chore: comment

--- scripts/select_contributions_refs.sql
+++ scripts/select_contributions_refs.sql
@@ -1,3 +1,5 @@
+-- Script pour Rémi: liste des contenus liés pour chaque contribution 
+
 select da."source" || '/' || da.slug as page_slug, da.cdtn_id as page_cdtn_id, dr."source" || '/' || dr.slug as ref_slug, dr.cdtn_id as ref_cdtn_id
 from contribution.answer_cdtn_references acr
 inner join public.documents dr on dr.cdtn_id = acr.cdtn_id
