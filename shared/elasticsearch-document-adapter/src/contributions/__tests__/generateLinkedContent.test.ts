d36c6d3667d355ccb7e509d0c6b3a5f0ffa1d908
Martial Maillot <martial.maillot@gmail.com>
2023-11-30 15:48:57 +0100

    fix: description sur les contenus liés

---
+++ shared/elasticsearch-document-adapter/src/contributions/__tests__/generateLinkedContent.test.ts
@@ -0,0 +1,99 @@
+import { generateLinkedContent } from "../generateLinkedContent";
+import { Breadcrumbs } from "../../breadcrumbs";
+import { fetchLinkedContent, LinkedContentLight } from "../fetchLinkedContent";
+import { ContributionElasticDocumentLightRelatedContent } from "../generate";
+
+jest.mock("../fetchLinkedContent");
+
+const breadcrumbs: Breadcrumbs[] = [
+  {
+    label: "Thème 1",
+    position: 0,
+    slug: "theme-1",
+  },
+  { label: "Thème 2", position: 0, slug: "theme-2" },
+];
+
+describe("generateLinkedContent", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("un contenu lié autres qu'une contribution personnalisée", () => {
+    it("doit retourner les infos du document depuis la BDD", async () => {
+      const dataFetchLinkedContent: LinkedContentLight = {
+        cdtnId: "123456",
+        title: "Titre",
+        slug: "slug",
+        description: "description",
+        source: "information",
+        isPublished: true,
+        isAvailable: true,
+      };
+      (fetchLinkedContent as jest.Mock).mockResolvedValue(
+        dataFetchLinkedContent
+      );
+
+      const linkedContent = await generateLinkedContent(
+        [],
+        1,
+        [
+          {
+            cdtnId: "12345",
+          },
+        ],
+        () => breadcrumbs,
+        {}
+      );
+
+      expect(linkedContent).toEqual({
+        linkedContent: [
+          {
+            breadcrumbs: breadcrumbs,
+            description: "description",
+            slug: "slug",
+            source: "information",
+            title: "Titre",
+          },
+        ],
+      });
+    });
+  });
+
+  describe("un contenu lié qui est une contribution personnalisée", () => {
+    it("doit retourner les infos de la contribution générée", async () => {
+      const contribution = {
+        cdtnId: "678910",
+        breadcrumbs: breadcrumbs,
+        description: "description contrib",
+        source: "contributions",
+        slug: "slug-contrib",
+        title: "title contrib",
+      } as ContributionElasticDocumentLightRelatedContent;
+
+      const linkedContent = await generateLinkedContent(
+        [contribution],
+        1,
+        [
+          {
+            cdtnId: "678910",
+          },
+        ],
+        () => breadcrumbs,
+        {}
+      );
+
+      expect(linkedContent).toEqual({
+        linkedContent: [
+          {
+            breadcrumbs: breadcrumbs,
+            description: "description contrib",
+            slug: "slug-contrib",
+            source: "contributions",
+            title: "title contrib",
+          },
+        ],
+      });
+    });
+  });
+});
