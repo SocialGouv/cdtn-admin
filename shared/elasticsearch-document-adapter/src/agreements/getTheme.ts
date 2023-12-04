863fb2550961bbc1e3583f8a54ecef268f1db177
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-12-04 10:47:35 +0100

    fix: migrations

--- shared/elasticsearch-document-adapter/src/agreements/getTheme.ts
+++ shared/elasticsearch-document-adapter/src/agreements/getTheme.ts
@@ -3,7 +3,5 @@ import { ContributionElasticDocument } from "@shared/types";
 export function getTheme(
   data: ContributionElasticDocument
 ): string | undefined {
-  return Array.isArray(data.breadcrumbs) && data.breadcrumbs.length > 0
-    ? data.breadcrumbs[0].label
-    : undefined;
+  return data.breadcrumbs.length > 0 ? data.breadcrumbs[0].label : undefined;
 }
