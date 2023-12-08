ed3edc81a288f8cea896f7bdffc2f9d43bd3a5d7
Victor <victor.zeinstra@gmail.com>
2023-12-08 16:02:50 +0100

    feat: add CC 3248 highlight

---
+++ targets/hasura/migrations/default/1702046689327_add-CC-3248-highlight/up.sql
@@ -0,0 +1,4 @@
+update documents
+set "document" = jsonb_set("document", '{highlight}','{"title": "A noter : les informations données sur cette page sont applicables depuis le 01/01/2024","content": "Cette convention collective est entrée en vigueur le 01/01/2024 suite au regroupement des conventions locales de la métallurgie ainsi que de la convention spécifique aux ingénieurs et cadres qui étaient applicables jusqu’au 31/12/2023.", "searchInfo": "Nouvelle convention collective en vigueur issue du regroupement des conventions locales de la métallurgie ainsi que de la convention spécifique aux ingénieurs et cadres"}', true)
+where "source" = 'conventions_collectives'
+and split_part(slug, '-', 1) = '3248'
