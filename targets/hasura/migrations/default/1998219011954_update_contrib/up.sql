816f510925f96b9ab545a07a310004fb3262283d
carolineBda <caroline.bourdeu@gmail.com>
2023-12-12 10:07:01 +0100

    try fix migration

--- targets/hasura/migrations/default/1998219011954_update_contrib/up.sql
+++
@@ -1,8 +0,0 @@
-update documents
-set initial_id = a.id
-from contribution.answers a
-    inner join contribution.questions q on q.id = a.question_id
-where documents."source" = 'contributions'
-    and a.agreement_id = coalesce(documents."document"->'answers'->'conventionAnswer'->>'idcc', '0000')
-    and q."content" = documents."title"
-    and q.id in ('b4c9c59f-20cc-462d-8bcd-70f34dcbc813', '99fc3644-fce1-4e48-bf5f-d59cf1d6d1ae', 'ad1c2bd2-8f37-450e-bf4a-bb8fb50ea2c7')
