2fa9a2fae19209d5f3c0d2f1866616efbfd7fb54
carolineBda <caroline.bourdeu@gmail.com>
2023-12-12 11:47:46 +0100

    clean migrations !

--- targets/hasura/migrations/default/1701783161587_alter_table_contribution_answers_add_column_messageNothingNoCDT/down.sql
+++
@@ -1,4 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "contribution"."answers" add column "message_block_generic_no_CDT" text
---  null;
--- targets/hasura/migrations/default/1701783161587_alter_table_contribution_answers_add_column_messageNothingNoCDT/up.sql
+++
@@ -1,2 +0,0 @@
-alter table "contribution"."answers" add column "message_block_generic_no_CDT" text
- null;
