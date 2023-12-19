05c9d05c8b01c879be08d668967fa267dc45bc49
Victor <victor.zeinstra@gmail.com>
2023-12-19 15:33:38 +0100

    feat: ajout du mock

--- targets/ingester/src/transform/agreements/index.ts
+++ targets/ingester/src/transform/agreements/index.ts
@@ -6,8 +6,106 @@ import { loadAgreements } from "../../lib/data-loaders";
 import { formatIdcc } from "../../lib/formatIdcc";
 import getAgreementsWithHighlight from "./agreementsWithHighlight";
 
+const metaIdccs = [
+  54, 650, 714, 822, 827, 828, 829, 836, 860, 863, 878, 887, 898, 899, 911, 914,
+  920, 923, 930, 934, 937, 943, 948, 965, 979, 984, 1007, 1059, 1159, 1164,
+  1274, 1315, 1353, 1365, 1369, 1387, 1472, 1525, 1560, 1564, 1572, 1576, 1577,
+  1578, 1592, 1604, 1626, 1627, 1628, 1634, 1635, 1732, 1813, 1867, 1885, 1902,
+  1912, 1960, 1966, 1967, 2003, 2126, 2221, 2266, 2294, 2489, 2542, 2579, 2615,
+  2630, 2700, 2755, 2980, 2992, 3053, 3209, 3231,
+];
+
 export default async function getAgreementDocuments() {
-  const agreements = await loadAgreements();
+  const agreements = (await loadAgreements()).flatMap((agreement) => {
+    if (agreement.num === 3248) {
+      return [
+        {
+          ...agreement,
+          active: true,
+          synonymes: [
+            "54",
+            "650",
+            "714",
+            "822",
+            "827",
+            "828",
+            "829",
+            "836",
+            "860",
+            "863",
+            "878",
+            "887",
+            "898",
+            "899",
+            "911",
+            "914",
+            "920",
+            "923",
+            "930",
+            "934",
+            "937",
+            "943",
+            "948",
+            "965",
+            "979",
+            "984",
+            "1007",
+            "1059",
+            "1159",
+            "1164",
+            "1274",
+            "1315",
+            "1353",
+            "1365",
+            "1369",
+            "1387",
+            "1472",
+            "1525",
+            "1560",
+            "1564",
+            "1572",
+            "1576",
+            "1577",
+            "1578",
+            "1592",
+            "1604",
+            "1626",
+            "1627",
+            "1628",
+            "1634",
+            "1635",
+            "1732",
+            "1813",
+            "1867",
+            "1885",
+            "1902",
+            "1912",
+            "1960",
+            "1966",
+            "1967",
+            "2003",
+            "2126",
+            "2221",
+            "2266",
+            "2294",
+            "2489",
+            "2542",
+            "2579",
+            "2615",
+            "2630",
+            "2700",
+            "2755",
+            "2980",
+            "2992",
+            "3053",
+            "3209",
+            "3231",
+          ],
+        },
+      ];
+    }
+    return !metaIdccs.find((num) => num === agreement.num) ? [agreement] : [];
+  });
 
   // Pour éviter qu'on perdre l'highlight lorsque l'ingester va ré-écrire les documents dans la db.
   const agreementsWithHighlight = await getAgreementsWithHighlight();
