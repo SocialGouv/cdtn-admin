906c9962e93442375768843100997467279cc335
Victor <victor.zeinstra@gmail.com>
2023-12-20 16:13:34 +0100

    fix: breadcrumb ordering algorithme

---
+++ shared/elasticsearch-document-adapter/src/__tests__/breadcrumbs.test.ts
@@ -0,0 +1,38 @@
+import { getMainBreadcrumb } from "../breadcrumbs";
+import { Breadcrumbs } from "@shared/types";
+
+describe("getMainBreadcrumb", () => {
+  test("Vérifier que le breadcrumbs avec la plus petite position remonte", () => {
+    const breadcrumbs1: Breadcrumbs[] = [
+      {
+        label: "label1",
+        position: 1,
+        slug: "slug1",
+      },
+      {
+        label: "label2",
+        position: 2,
+        slug: "slug2",
+      },
+    ];
+    const breadcrumbs2: Breadcrumbs[] = [
+      {
+        label: "label2",
+        position: 2,
+        slug: "slug2",
+      },
+      {
+        label: "label3",
+        position: 3,
+        slug: "slug3",
+      },
+      {
+        label: "label4",
+        position: 4,
+        slug: "slug4",
+      },
+    ];
+    const result = getMainBreadcrumb([breadcrumbs1, breadcrumbs2]);
+    expect(result).toEqual(breadcrumbs1);
+  });
+});
