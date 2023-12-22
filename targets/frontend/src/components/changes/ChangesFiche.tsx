cddaecf309339c1d62d1e85d3c84b78ec1f6eb7f
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-12-22 16:03:34 +0100

    fix: conv

--- targets/frontend/src/components/changes/ChangesFiche.tsx
+++ targets/frontend/src/components/changes/ChangesFiche.tsx
@@ -3,70 +3,82 @@ import { DocumentInfoWithCdtnRef } from "@shared/types";
 import slugify from "@socialgouv/cdtn-slugify";
 import { getRouteBySource } from "@socialgouv/cdtn-sources";
 import { theme } from "src/theme";
+import { styled } from "@mui/system";
 
 type Props = {
   documents: DocumentInfoWithCdtnRef[];
   type: "vdd" | "travail-data";
 };
 
-const palettes = {
-  contributions: "#5bc0eb",
-  prequalified: "#fde74c",
-  themes: "#9bc53d",
-} as any;
-
 export function ChangesFiche({ documents, type }: Props): JSX.Element {
   return (
     <div>
       {documents.map((doc) => (
-        <div
-          key={doc.id}
-          style={{
-            marginBottom: "1rem",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <Chip
-            label={doc.source}
-            style={{ backgroundColor: palettes[doc.source], width: "125px" }}
-          />
-          <div style={{ marginLeft: "20px" }}>
-            <a
+        <DocumentContainer key={doc.id}>
+          <DocumentChip label={doc.source} source={doc.source} />
+          <LinkContainer>
+            <DocumentLink
               target="_blank"
               href={`https://code.travail.gouv.fr/${getRouteBySource(
                 doc.source
               )}/${slugify(doc.title)}`}
             >
               {doc.title}
-            </a>
+            </DocumentLink>
 
-            <div>
-              <a
-                target="_blank"
-                style={{
-                  color: theme.colors.muted,
-                  fontSize: "0.8rem",
-                  lineHeight: 1,
-                }}
-                href={
-                  type === "vdd"
-                    ? doc.url ??
-                      `https://code.travail.gouv.fr/fiche-service-public/${slugify(
-                        doc.ref.title
-                      )}`
-                    : `https://code.travail.gouv.fr/fiche-ministere-travail/${slugify(
-                        doc.ref.title
-                      )}`
-                }
-              >
-                {doc.ref.id} - {doc.ref.title}
-              </a>
-            </div>
-          </div>
-        </div>
+            <ReferenceLink
+              target="_blank"
+              href={
+                type === "vdd"
+                  ? doc.url ??
+                    `https://code.travail.gouv.fr/fiche-service-public/${slugify(
+                      doc.ref.title
+                    )}`
+                  : `https://code.travail.gouv.fr/fiche-ministere-travail/${slugify(
+                      doc.ref.title
+                    )}`
+              }
+            >
+              {doc.ref.id} - {doc.ref.title}
+            </ReferenceLink>
+          </LinkContainer>
+        </DocumentContainer>
       ))}
     </div>
   );
 }
+
+const palettes = {
+  contributions: "#5bc0eb",
+  prequalified: "#fde74c",
+  themes: "#9bc53d",
+} as const;
+
+const DocumentContainer = styled("div")({
+  marginBottom: "1rem",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+});
+
+const DocumentChip: any = styled(Chip)(({ source }: any) => ({
+  backgroundColor: palettes[source as keyof typeof palettes],
+  width: "125px",
+}));
+
+const LinkContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  marginLeft: "20px",
+});
+
+const DocumentLink = styled("a")({
+  marginBottom: "10px",
+  width: "fit-content",
+});
+
+const ReferenceLink = styled("a")({
+  color: theme.colors.muted,
+  fontSize: "0.8rem",
+  width: "fit-content",
+});
