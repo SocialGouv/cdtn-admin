e81b84daa9e1ed4110c5d16d7a9ddf2bd0d4f858
Martial Maillot <martial.maillot@gmail.com>
2024-01-11 15:21:13 +0100

    fix: migration de la question 39 et 51

---
+++ targets/hasura/migrations/default/1704982796000_redeploy_39_51/up.sql
@@ -0,0 +1,8 @@
+update documents
+set initial_id = a.id
+from contribution.answers a
+    inner join contribution.questions q on q.id = a.question_id
+where documents."source" = 'contributions'
+    and a.agreement_id = coalesce(documents."document"->'answers'->'conventionAnswer'->>'idcc', '0000')
+    and q."content" = documents."title"
+    and q.order in (39, 51);
