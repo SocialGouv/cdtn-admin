517b96ed9cee0099278558190d881e7352669f8e
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-01-26 14:57:11 +0100

    fix: remove antislash n

--- targets/hasura/migrations/default/1706260191591_remove_antislash_n/up.sql
+++ targets/hasura/migrations/default/1706260191591_remove_antislash_n/up.sql
@@ -1,6 +1,6 @@
 UPDATE contribution.answers
 SET description = REPLACE(description, '\n', '')
-WHERE description LIKE '%\n%';
+WHERE description LIKE E'%\n%';
 
 UPDATE public.documents d
 SET "document" = jsonb_set(
@@ -9,4 +9,4 @@ SET "document" = jsonb_set(
     jsonb_build_object('description', REPLACE((d."document"->'description'->>'description')::text, '\n', ''))
 )
 WHERE d."document"->'description' is not null
-  AND (d."document"->'description'->>'description')::text LIKE '%\n%';
+  AND (d."document"->'description'->>'description')::text LIKE E'%\n%';
