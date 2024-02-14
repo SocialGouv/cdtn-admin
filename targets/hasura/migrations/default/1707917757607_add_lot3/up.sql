0088b5def6963ca2f972b8e10f971d40b0642e95
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-02-14 16:29:48 +0100

    fix: lot 4

--- targets/hasura/migrations/default/1707917757607_add_lot3/up.sql
+++ targets/hasura/migrations/default/1707917757607_add_lot3/up.sql
@@ -4,9 +4,9 @@ set initial_id = a.id
 inner join contribution.questions q on q.id = a.question_id
 where documents."source" = 'contributions'
   and (
-  (split_part(documents."text", ' ', 1) ~ '^\d+(\.\d+)?$' and
+  (pg_input_is_valid(split_part(documents."text", ' ', 1), 'int') and
       a.agreement_id = split_part(documents."text", ' ', 1))
-      or a.agreement_id = '0000'
+      or (not pg_input_is_valid(split_part(documents."text", ' ', 1), 'int') and a.agreement_id = '0000')
   )
   and q."content" = documents."title"
   and q.order in (1, 3, 8, 13, 17, 34, 43, 44, 45, 46, 52, 53);
