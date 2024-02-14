e27014e5877de15f1fd693d77fe75d90d57104f3
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-02-14 15:59:44 +0100

    fix: lot 4

---
+++ targets/hasura/migrations/default/1707922771238_add_lot3_generic/up.sql
@@ -0,0 +1,8 @@
+update documents
+set initial_id = a.id
+  from contribution.answers a
+inner join contribution.questions q on q.id = a.question_id
+where documents."source" = 'contributions'
+  and a.agreement_id = '0000'
+  and q."content" = documents."title"
+  and q.order in (1, 3, 8, 13, 17, 34, 43, 44, 45, 46, 52, 53);
