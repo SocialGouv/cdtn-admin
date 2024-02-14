a07549e50849188143958e3b65961de3615cc936
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-02-14 16:15:35 +0100

    fix: lot 4

--- targets/hasura/migrations/default/1707917757607_add_lot3/up.sql
+++ targets/hasura/migrations/default/1707917757607_add_lot3/up.sql
@@ -3,6 +3,10 @@ set initial_id = a.id
   from contribution.answers a
 inner join contribution.questions q on q.id = a.question_id
 where documents."source" = 'contributions'
-  and a.agreement_id = split_part(documents."text", ' ', 1)
+  and (
+  (split_part(documents."text", ' ', 1) ~ '^\d+(\.\d+)?$' and
+      a.agreement_id = split_part(documents."text", ' ', 1))
+      or a.agreement_id = '0000'
+  )
   and q."content" = documents."title"
   and q.order in (1, 3, 8, 13, 17, 34, 43, 44, 45, 46, 52, 53);
