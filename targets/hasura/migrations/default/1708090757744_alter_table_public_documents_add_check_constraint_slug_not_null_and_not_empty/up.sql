b68ffbb01d13b7693fe414b3adfccd4ccbb490cc
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-02-16 14:44:27 +0100

    fix: doc

--- targets/hasura/migrations/default/1708090757744_alter_table_public_documents_add_check_constraint_slug_not_null_and_not_empty/up.sql
+++ targets/hasura/migrations/default/1708090757744_alter_table_public_documents_add_check_constraint_slug_not_null_and_not_empty/up.sql
@@ -1,1 +1,1 @@
-alter table "public"."documents" add constraint "slug_not_null_and_not_empty" check (slug IS NOT NULL AND slug <> '');
+alter table "public"."documents" add constraint "slug_not_null_and_not_empty" check (slug IS NOT NULL AND slug <> ''::text);
