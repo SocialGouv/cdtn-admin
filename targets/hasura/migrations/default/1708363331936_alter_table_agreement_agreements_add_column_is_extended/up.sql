e15d02f98e1089579656d2299796462a8fdc8e80
Victor <victor.zeinstra@gmail.com>
2024-02-21 16:04:27 +0100

    feat: set is_extended bool

--- targets/hasura/migrations/default/1708363331936_alter_table_agreement_agreements_add_column_is_extended/up.sql
+++ targets/hasura/migrations/default/1708363331936_alter_table_agreement_agreements_add_column_is_extended/up.sql
@@ -1,2 +1,5 @@
-alter table "agreement"."agreements" add column "is_extended" boolean
- not null default 'false';
+alter table "agreement"."agreements"
+add column "is_extended" boolean not null default 'false';
+update agreement.agreements
+set is_extended = true
+where id in ('0413', '0029', '2420');
