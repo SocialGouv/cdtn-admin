abccaa4626baaa983e4189b266b9bc2a2fa1647e
Victor <victor.zeinstra@gmail.com>
2024-03-05 11:06:55 +0100

    chore: clean

--- targets/hasura/migrations/default/1708427376180_create_table_contribution_agreement_messages/up.sql
+++ targets/hasura/migrations/default/1708427376180_create_table_contribution_agreement_messages/up.sql
@@ -8,19 +8,19 @@ CREATE TABLE "contribution"."agreement_messages" (
 );
 insert into contribution.agreement_messages(agreement_id, content)
 values (
-                '0413',
-                '<p>Ces informations sont issues du Code du travail car les dispositions de votre convention collective n’ont pas été étendues. Ces dernières ne s''appliquent qu''aux entreprises adhérentes à l’une des organisations signataires de l''accord. Dans ce contexte, nous ne sommes pas en mesure d''identifier si elles s''appliquent ou non au sein de votre entreprise. Vous pouvez toutefois consulter votre convention collective <a href="https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635407" target="_blank">ici</a> dans le cas où elles s''appliquent à votre situation.</p>'
-        );
+        '0413',
+        '<p>Ces informations sont issues du Code du travail car les dispositions de votre convention collective n’ont pas été étendues. Ces dernières ne s''appliquent qu''aux entreprises adhérentes à l’une des organisations signataires de l''accord. Dans ce contexte, nous ne sommes pas en mesure d''identifier si elles s''appliquent ou non au sein de votre entreprise. Vous pouvez toutefois consulter votre convention collective <a href="https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635407" target="_blank">ici</a> dans le cas où elles s''appliquent à votre situation.</p>'
+);
 insert into contribution.agreement_messages(agreement_id, content)
 values (
-                '0029',
-                '<p>Ces informations sont issues du Code du travail car les dispositions de votre convention collective n’ont pas été étendues. Ces dernières ne s''appliquent qu''aux entreprises adhérentes à l’une des organisations signataires de l''accord. Dans ce contexte, nous ne sommes pas en mesure d''identifier si elles s''appliquent ou non au sein de votre entreprise. Vous pouvez toutefois consulter votre convention collective <a href="https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635234" target="_blank">ici</a> dans le cas où elles s''appliquent à votre situation.</p>'
-        );
+        '0029',
+        '<p>Ces informations sont issues du Code du travail car les dispositions de votre convention collective n’ont pas été étendues. Ces dernières ne s''appliquent qu''aux entreprises adhérentes à l’une des organisations signataires de l''accord. Dans ce contexte, nous ne sommes pas en mesure d''identifier si elles s''appliquent ou non au sein de votre entreprise. Vous pouvez toutefois consulter votre convention collective <a href="https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000005635234" target="_blank">ici</a> dans le cas où elles s''appliquent à votre situation.</p>'
+);
 insert into contribution.agreement_messages(agreement_id, content)
 values (
-                '2420',
-                '<p>Ces informations sont issues du Code du travail car les dispositions de votre convention collective n’ont pas été étendues. Ces dernières ne s''appliquent qu''aux entreprises adhérentes à l’une des organisations signataires de l''accord. Dans ce contexte, nous ne sommes pas en mesure d''identifier si elles s''appliquent ou non au sein de votre entreprise. Vous pouvez toutefois consulter votre convention collective <a href="https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000017941839" target="_blank">ici</a> dans le cas où elles s''appliquent à votre situation.</p>'
-        );
+        '2420',
+        '<p>Ces informations sont issues du Code du travail car les dispositions de votre convention collective n’ont pas été étendues. Ces dernières ne s''appliquent qu''aux entreprises adhérentes à l’une des organisations signataires de l''accord. Dans ce contexte, nous ne sommes pas en mesure d''identifier si elles s''appliquent ou non au sein de votre entreprise. Vous pouvez toutefois consulter votre convention collective <a href="https://www.legifrance.gouv.fr/affichIDCC.do?idConvention=KALICONT000017941839" target="_blank">ici</a> dans le cas où elles s''appliquent à votre situation.</p>'
+);
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 update contribution.answers
 set content_type = 'NOTHING'
