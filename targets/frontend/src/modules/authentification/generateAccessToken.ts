53a3b24d4491ff6a185200ed0b9ba8f9829c6551
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-03-28 15:32:42 +0100

    fix: bug

--- targets/frontend/src/modules/authentification/generateAccessToken.ts
+++ targets/frontend/src/modules/authentification/generateAccessToken.ts
@@ -1,7 +1,20 @@
-import { AuthJwtRefreshError } from "./errors";
+import { AuthGqlError, AuthJwtRefreshError, AuthUserDeleted } from "./errors";
 import { UserStoredInJwt, generateJwtToken, verifyToken } from "./jwt";
 import { JWT_TOKEN_EXPIRES } from "src/config";
 import { UserSignedIn } from "./signIn";
+import { gqlClient } from "@shared/utils";
+
+const getUserByMailQuery = `
+  query login($email: citext!) {
+    auth_users(where: {email: {_eq: $email}}) {
+      isDeleted
+    }
+  }
+`;
+
+interface GetUserByMailHasuraResult {
+  auth_users: { isDeleted: boolean }[];
+}
 
 export const generateNewAccessToken = async (
   user: UserSignedIn
@@ -22,7 +35,28 @@ export const generateNewAccessToken = async (
     role: user.role,
   };
 
-  // verifier en db que l'utilisateur n'est pas deleted
+  // Sécurité en vérifiant que l'utilisateur n'a pas été supprimé pour lui générer un nouveau token.
+  const result = await gqlClient()
+    .query<GetUserByMailHasuraResult>(getUserByMailQuery, {
+      email: user.email,
+    })
+    .toPromise();
+
+  if (result.error || !result.data?.auth_users[0]) {
+    throw new AuthGqlError({
+      cause: result.error,
+      message: "User not find",
+      name: "AUTH_GQL_ERROR",
+    });
+  }
+
+  if (result.data.auth_users[0].isDeleted) {
+    throw new AuthUserDeleted({
+      message: `${user.email} has been deleted`,
+      name: "AUTH_USER_DELETED",
+      cause: null,
+    });
+  }
 
   const accessTokenGenerated = generateJwtToken(userToSave, JWT_TOKEN_EXPIRES);
 
