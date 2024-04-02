52f49f9adf421d3e220beab8e4726de8b4b430b0
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-04-02 12:39:49 +0200

    fix: test

--- targets/frontend/src/modules/emails/sendmail.ts
+++ targets/frontend/src/modules/emails/sendmail.ts
@@ -2,12 +2,21 @@ import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
 
 export default function sendmail(mailOptions: Mail.Options) {
+  if (
+    !process.env.SMTP_EMAIL_USER ||
+    !process.env.SMTP_EMAIL_PASSWORD ||
+    !process.env.SMTP_URL
+  ) {
+    throw new Error(
+      "SMTP_EMAIL_USER, SMTP_EMAIL_PASSWORD and SMTP_URL must be set"
+    );
+  }
   const transport = nodemailer.createTransport({
     auth: {
-      pass: process.env.SMTP_EMAIL_PASSWORD ?? "pass",
-      user: process.env.SMTP_EMAIL_USER ?? "email",
+      pass: process.env.SMTP_EMAIL_PASSWORD,
+      user: process.env.SMTP_EMAIL_USER,
     },
-    host: process.env.SMTP_URL ?? "smtp.url",
+    host: process.env.SMTP_URL,
     port: 587,
   });
   return transport.sendMail(mailOptions).finally(() => transport.close());
