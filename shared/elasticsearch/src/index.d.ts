c6d8f68dc3fd12d670e8b554687bacd7cc609ac5
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-04-02 09:56:44 +0200

    fix: naming

--- shared/elasticsearch/src/index.d.ts
+++ shared/elasticsearch/src/index.d.ts
@@ -1,11 +1,9 @@
-declare module "@socialgouv/cdtn-elasticsearch" {
-  export const createIndex: any;
-  export const deleteOldIndex: any;
-  export const documentMapping: any;
-  export const DOCUMENTS: string;
-  export const indexDocumentsBatched: any;
-  export const SUGGESTIONS: string;
-  export const vectorizeDocument: any;
-  export const version: any;
-  export const suggestionMapping: any;
-}
+export const createIndex: any;
+export const deleteOldIndex: any;
+export const documentMapping: any;
+export const DOCUMENTS: string;
+export const indexDocumentsBatched: any;
+export const SUGGESTIONS: string;
+export const vectorizeDocument: any;
+export const version: any;
+export const suggestionMapping: any;
