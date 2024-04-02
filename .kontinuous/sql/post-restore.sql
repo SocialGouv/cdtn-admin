ec3983ba7c69a8f6736f4687bb3441fcc4375715
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-04-02 16:10:40 +0200

    fix: test

--- .kontinuous/sql/post-restore.sql
+++ .kontinuous/sql/post-restore.sql
@@ -3,7 +3,7 @@ UPDATE auth.users as u
 SET 
   password = '$argon2i$v=19$m=4096,t=3,p=1$n9eoWSv+5sCgc7SjB5hLig$iBQ7NzrHHLkJSku/dCetNs+n/JI1CMdkWaoZsUekLU8',
   email = u.id || '@travail.gouv.fr',
-  name = u.id
+  name = u.id;
 
 -- Add a fake user
 INSERT INTO auth.users (email, password, name, role, is_active, id)
@@ -14,7 +14,7 @@ VALUES (
     'super',
     TRUE,
     'd8b11bd2-dd16-4632-b5de-0e7021faadeb'
-  )
+  );
 
 -- Remove all data from audit tables
 TRUNCATE audit.logged_actions;
