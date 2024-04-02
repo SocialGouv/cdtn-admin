0d9d72ac381a94ead240738b8cbdfe696dc9965d
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-04-02 19:39:05 +0200

    fix: merge

--- targets/frontend/src/pages/api/graphql.ts
+++ targets/frontend/src/pages/api/graphql.ts
@@ -15,6 +15,9 @@ const proxy = createProxyMiddleware({
   prependPath: false,
   target:
     process.env.HASURA_GRAPHQL_ENDPOINT ?? "http://localhost:8080/v1/graphql",
+  followRedirects: true,
+  ws: true,
+  xfwd: true,
 });
 
 export default proxy;
