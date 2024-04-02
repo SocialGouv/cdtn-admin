2e81cc1a250a80db0c03defa963922cae857ec15
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-04-02 15:59:29 +0200

    fix: test

--- .kontinuous/sql/post-restore.sql
+++ .kontinuous/sql/post-restore.sql
@@ -5,9 +5,6 @@ SET
   email = u.id || '@travail.gouv.fr',
   name = u.id
 
--- Disable triggers
-ALTER TABLE auth.users DISABLE TRIGGER USER;
-
 -- Add a fake user
 INSERT INTO auth.users (email, password, name, role, is_active, id)
 VALUES (
@@ -18,28 +15,15 @@ VALUES (
     TRUE,
     'd8b11bd2-dd16-4632-b5de-0e7021faadeb'
   )
-    
--- Create empty audit logs
-CREATE TABLE IF NOT EXISTS audit.logged_actions (
-  event_id bigserial primary key,
-  schema_name text not null,
-  table_name text not null,
-  relid oid not null,
-  session_user_name text,
-  hasura_user jsonb,
-  action_tstamp_tx TIMESTAMP WITH TIME ZONE NOT NULL,
-  action_tstamp_stm TIMESTAMP WITH TIME ZONE NOT NULL,
-  action_tstamp_clock TIMESTAMP WITH TIME ZONE NOT NULL,
-  transaction_id bigint,
-  application_name text,
-  client_addr inet,
-  client_port integer,
-  client_query text,
-  action TEXT NOT NULL CHECK (action IN ('I', 'D', 'U', 'T')),
-  row_data jsonb,
-  changed_fields jsonb,
-  statement_only boolean not null
-);
 
--- Enable triggers
-ALTER TABLE auth.users ENABLE TRIGGER USER;
+-- Remove all data from audit tables
+TRUNCATE audit.logged_actions;
+
+-- Kill all connections !
+-- Make all connected services restart !
+-- Hasura migration will be re-applyed.
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE pid <> pg_backend_pid()
+  AND datname = '${PGDATABASE}';
+
