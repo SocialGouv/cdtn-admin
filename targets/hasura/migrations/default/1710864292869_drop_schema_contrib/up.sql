b5c68653231273e7b6e7aee35856af7ab45b28ae
Victor <victor.zeinstra@gmail.com>
2024-04-03 15:34:11 +0200

    chore: clean

--- targets/hasura/migrations/default/1710864292869_drop_schema_contrib/up.sql
+++ targets/hasura/migrations/default/1710864292869_drop_schema_contrib/up.sql
@@ -1,13 +1,13 @@
 drop schema "contrib" cascade;
--- delete from contribution.answer_cdtn_references
--- where cdtn_id in (
--- 	select cdtn_id
--- 	FROM public.documents
--- 	WHERE source = 'contributions'
--- 	AND document ? 'split'
--- 	AND is_available = false
--- );
--- delete FROM public.documents
--- WHERE source = 'contributions'
--- AND document ? 'split'
--- AND is_available = false;
+delete from contribution.answer_cdtn_references
+where cdtn_id in (
+	select cdtn_id
+	FROM public.documents
+	WHERE source = 'contributions'
+	AND document ? 'split'
+	AND is_available = false
+);
+delete FROM public.documents
+WHERE source = 'contributions'
+AND document ? 'split'
+AND is_available = false;
