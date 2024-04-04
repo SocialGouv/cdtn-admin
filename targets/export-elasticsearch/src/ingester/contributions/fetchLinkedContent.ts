917574c135f3f74ae445d2b10ca5eb21375787b7
Victor Zeinstra <zeinstra.victor@gmail.com>
2024-04-04 16:28:22 +0200

    feat: desinstaller les anciennes contributions de l'admin (#1326)
    * feat: desinstaller les anciennes contributions de l'admin
    
    * chore: clean
    
    * chore: clean
    
    * chore: yarn lock
    
    * chore: clean
    
    * chore: clean
    
    * feat: remove all old contrib
    
    * fix: remove old contrib from alert cli
    
    * remove package contribution
    
    * chore: clean
    
    ---------
    
    Co-authored-by: Victor <victor.zeinstra@gmail.com>

--- targets/export-elasticsearch/src/ingester/contributions/fetchLinkedContent.ts
+++ targets/export-elasticsearch/src/ingester/contributions/fetchLinkedContent.ts
@@ -31,8 +31,10 @@ export async function fetchLinkedContent(
   questionIndex: number,
   idcc: string
 ): Promise<LinkedContentLight | undefined> {
-  const HASURA_GRAPHQL_ENDPOINT = context.get("cdtnAdminEndpoint") || "http://localhost:8080/v1/graphql";
-  const HASURA_GRAPHQL_ENDPOINT_SECRET = context.get("cdtnAdminEndpointSecret") ||"admin1";
+  const HASURA_GRAPHQL_ENDPOINT =
+    context.get("cdtnAdminEndpoint") || "http://localhost:8080/v1/graphql";
+  const HASURA_GRAPHQL_ENDPOINT_SECRET =
+    context.get("cdtnAdminEndpointSecret") || "admin1";
   const res = await gqlClient({
     graphqlEndpoint: HASURA_GRAPHQL_ENDPOINT,
     adminSecret: HASURA_GRAPHQL_ENDPOINT_SECRET,
