740abdd37c92e6b2ebb6e4417adae8b8882e8b04
Caroline <4971715+carolineBda@users.noreply.github.com>
2024-04-15 13:03:15 +0200

    fix(outils de contrib): nettoyage de fichiers non utilisés (#1360)

--- targets/ingester/src/lib/fetchContributions/__mocks__/AgreementMock.ts
+++
@@ -1,9 +0,0 @@
-import { IndexedAgreement } from "@socialgouv/kali-data-types";
-import type { AgreementRepository } from "../AgreementRepository";
-import InputAgreement from "./input-agreements.json";
-
-export class AgreementMock implements AgreementRepository {
-  async fetchAll(): Promise<IndexedAgreement[]> {
-    return Promise.resolve(InputAgreement as unknown as IndexedAgreement[]);
-  }
-}
