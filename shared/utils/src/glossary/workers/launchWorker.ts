60d50b2cb1f3563a964dd41bd8dff1e4c5b6b67c
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-06-06 14:55:47 +0200

    fix: glossary

--- shared/utils/src/glossary/workers/launchWorker.ts
+++ shared/utils/src/glossary/workers/launchWorker.ts
@@ -1,6 +1,7 @@
 import { Glossary } from "@socialgouv/cdtn-types";
 import { Worker } from "worker_threads";
 import path from "path";
+import { getMonorepoRoot } from "./utils";
 
 export type GlossaryWorkerData = {
   glossary: Glossary;
@@ -12,9 +13,12 @@ export const addGlossaryContentWorker = async (
   workerData: GlossaryWorkerData
 ): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const packagePath = path.dirname(require.resolve("@shared/utils"));
+    const rootPath = getMonorepoRoot(__dirname, "cdtn-admin");
     const worker = new Worker(
-      path.join("../", packagePath, "/glossary/workers/runGlossaryProcess.js"),
+      path.join(
+        rootPath,
+        "/shared/utils/build/src/glossary/workers/runGlossaryProcess.js"
+      ),
       {
         workerData,
       }
