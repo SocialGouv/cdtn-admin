cbd97516f9e31ecd7a6979049a2f7e71d277c5ff
victor <victor.zeinstra@gmail.com>
2024-06-06 16:29:57 +0200

    chore: clean

--- targets/frontend/src/components/contributions/publication/isPublished.ts
+++ targets/frontend/src/components/contributions/publication/isPublished.ts
@@ -1,11 +1,9 @@
-import { AnswerStatus, ExportStatus } from "../type";
-
 export const isPublished = ({
   statusDate,
   exportDate,
 }: {
   statusDate: string;
-  exportDate?: string;
+  exportDate: string;
 }): boolean => {
   return new Date(exportDate).getTime() > new Date(statusDate).getTime();
 };
