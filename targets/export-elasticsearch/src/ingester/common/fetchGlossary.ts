e2ab2c62bef25f27088f107f4af9d77ba40e5ec3
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-06-07 19:49:33 +0200

    fix: done

--- targets/export-elasticsearch/src/ingester/common/fetchGlossary.ts
+++ targets/export-elasticsearch/src/ingester/common/fetchGlossary.ts
@@ -1,11 +1,24 @@
-import { fetchGlossary } from "@shared/utils";
+import { gqlClient } from "@shared/utils";
 import { context } from "../context";
 import { Glossary } from "@socialgouv/cdtn-types";
+import { getGlossaryQuery } from "../../repositories/graphql";
 
 export const getGlossary = async (): Promise<Glossary> => {
   const graphqlEndpoint: string =
     context.get("cdtnAdminEndpoint") || "http://localhost:8080/v1/graphql";
   const adminSecret: string =
     context.get("cdtnAdminEndpointSecret") || "admin1";
-  return await fetchGlossary({ adminSecret, graphqlEndpoint });
+  const result = await gqlClient({
+    graphqlEndpoint,
+    adminSecret,
+  })
+    .query<{ glossary: Glossary }>(getGlossaryQuery, {})
+    .toPromise();
+  if (result.error || !result.data) {
+    console.error(result.error);
+    throw new Error(
+      `Error fetching glossary => ${JSON.stringify(result.error)}`
+    );
+  }
+  return result.data.glossary;
 };
