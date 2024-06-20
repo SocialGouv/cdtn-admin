2b9da39d9d3e7a08777cfce9142b64068a8c32b0
victor <victor.zeinstra@gmail.com>
2024-06-20 15:14:04 +0200

    chore: review

--- targets/export-elasticsearch/src/ingester/contributions/__tests__/filterContributionToPublish.test.ts
+++ targets/export-elasticsearch/src/ingester/contributions/__tests__/filterContributionToPublish.test.ts
@@ -1,18 +1,81 @@
+import {
+  ContributionDocumentJson,
+  ContributionsAnswers,
+  DocumentElasticWithSource,
+} from "@socialgouv/cdtn-types";
 import { filterContributionDocumentsToPublish } from "../fetchContributionDocumentsToPublish";
+
+const contributionMock: ContributionsAnswers = {
+  id: "d61a3e21-9bf1-4199-b9d7-944c2382750c",
+  updatedAt: "2024-03-25T17:25:33.002376+00:00",
+  agreement: {
+    id: "id",
+    kali_id: "",
+    name: "",
+  },
+  cdtn_references: [],
+  content: "",
+  content_fiche_sp: {
+    initial_id: "",
+    document: {
+      date: "",
+      description: "",
+      raw: "",
+      referencedTexts: [],
+      url: "",
+    },
+  },
+  content_type: "ANSWER",
+  description: "",
+  kali_references: [],
+  legi_references: [],
+  message_block_generic_no_CDT: "",
+  other_references: [],
+  question: {
+    content: "",
+    id: "",
+    order: 1,
+  },
+  statuses: [],
+};
+
+const doc: DocumentElasticWithSource<ContributionDocumentJson> = {
+  id: "",
+  title: "",
+  excludeFromSearch: true,
+  slug: "",
+  source: "contributions",
+  text: "",
+  isPublished: true,
+  metaDescription: "",
+  references: [],
+  linkedContent: [],
+  questionIndex: 1,
+  questionName: "",
+  questionId: "",
+  genericAnswerId: "",
+  type: "cdt",
+  idcc: "",
+  refs: [],
+  breadcrumbs: [],
+  cdtnId: "",
+  contentType: "ANSWER",
+  description: "",
+  contribution: contributionMock,
+};
 
 describe("filterContributionDocumentsToPublish", () => {
   it("should send the list of contributions which have been updated after last export", () => {
     const result = filterContributionDocumentsToPublish([
       {
-        cdtn_id: "388f8d7860",
-        //@ts-ignore
+        ...doc,
+        cdtnId: "388f8d7860",
         export: {
+          id: "id",
           createdAt: "2024-06-03T08:10:03.697249+00:00",
         },
-        //@ts-ignore
         contribution: {
-          id: "d61a3e21-9bf1-4199-b9d7-944c2382750c",
-          updatedAt: "2024-03-25T17:25:33.002376+00:00",
+          ...contributionMock,
           statuses: [
             {
               status: "TO_PUBLISH",
@@ -27,15 +90,14 @@ describe("filterContributionDocumentsToPublish", () => {
   it("should send the list of contributions which have been updated after last export2", () => {
     const result = filterContributionDocumentsToPublish([
       {
-        cdtn_id: "388f8d7860",
-        //@ts-ignore
+        ...doc,
+        cdtnId: "388f8d7860",
         export: {
+          id: "id",
           createdAt: "2024-06-03T08:10:03.697249+00:00",
         },
-        //@ts-ignore
         contribution: {
-          id: "d61a3e21-9bf1-4199-b9d7-944c2382750c",
-          updatedAt: "2024-03-25T17:25:33.002376+00:00",
+          ...contributionMock,
           statuses: [
             {
               status: "TO_PUBLISH",
