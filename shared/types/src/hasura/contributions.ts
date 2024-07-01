58c9d5866d33cef8c64f1ebab177306e178743c2
carolineBda <caroline.bourdeu@gmail.com>
2024-07-01 16:49:45 +0200

    feat(contributions): sauvegarde de la date de mise à jour des contribs

--- shared/types/src/hasura/contributions.ts
+++ shared/types/src/hasura/contributions.ts
@@ -93,6 +93,7 @@ type ContributionDocumentJsonBasic = {
   questionId: string;
   description: string;
   idcc: string; // 0000 pour la générique, impossible d'être nulle
+  updated_at: Date;
 };
 
 export type ContributionDocumentJsonContent = ContributionDocumentJsonBasic & {
