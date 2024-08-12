b22c07bd33cbfafc52a859fa2ac32c7405978fc7
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-08-12 16:49:18 +0200

    fix: on tente

--- targets/hasura/migrations/default/1722934913490_create_table_public_document_exports/up.sql
+++ targets/hasura/migrations/default/1722934913490_create_table_public_document_exports/up.sql
@@ -1,11 +1,2 @@
+CREATE TABLE "public"."document_exports" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "export_id" uuid NOT NULL, "cdtn_id" text NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("export_id") REFERENCES "public"."export_es_status"("id") ON UPDATE restrict ON DELETE restrict, FOREIGN KEY ("cdtn_id") REFERENCES "public"."documents"("cdtn_id") ON UPDATE restrict ON DELETE restrict, UNIQUE ("cdtn_id"));
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
-CREATE TABLE "public"."document_exports" (
-    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
-    "export_id" uuid NOT NULL,
-    "cdtn_id" text NOT NULL,
-    PRIMARY KEY ("id"),
-    FOREIGN KEY ("export_id") REFERENCES "public"."export_es_status"("id") ON UPDATE restrict ON DELETE restrict,
-    FOREIGN KEY ("cdtn_id") REFERENCES "public"."documents"("cdtn_id") ON UPDATE restrict ON DELETE restrict,
-    UNIQUE ("export_id", "cdtn_id")
-);
