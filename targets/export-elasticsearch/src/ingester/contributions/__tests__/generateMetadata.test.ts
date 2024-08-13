976f40f56ab2c4d0a6bb96ef4af5da125c75ac52
Martial Maillot <martial.maillot@gmail.com>
2024-08-13 11:56:01 +0200

    test: ajout de cas de test

--- targets/export-elasticsearch/src/ingester/contributions/__tests__/generateMetadata.test.ts
+++ targets/export-elasticsearch/src/ingester/contributions/__tests__/generateMetadata.test.ts
@@ -1,51 +1,216 @@
 import {
-  DocumentElasticWithSource,
   AgreementDoc,
-  ContributionDocumentJson,
   Breadcrumb,
+  ContributionDocumentJson,
   ContributionMetadata,
+  DocumentElasticWithSource,
 } from "@socialgouv/cdtn-types";
 import { generateMetadata } from "../generateMetadata";
 
+const breadcrumbs: Breadcrumb[] = [
+  {
+    label: "Breadcrumb 1",
+    position: 1,
+    slug: "slug1",
+  },
+  {
+    label: "Breadcrumb 2",
+    position: 2,
+    slug: "slug2",
+  },
+];
+
 describe("generateMetadata", () => {
-  const ccns: Partial<DocumentElasticWithSource<AgreementDoc>>[] = [
-    {
-      num: 123,
-    },
-  ];
+  describe("Page non personnalisée", () => {
+    describe("avec un titre SEO personnalisé", () => {
+      const contribution: Partial<
+        DocumentElasticWithSource<ContributionDocumentJson>
+      > = {
+        idcc: "0000",
+        questionName: "Question",
+        seoTitle: "SEO Title",
+        description: "Description",
+      };
 
-  const contribution: Partial<
-    DocumentElasticWithSource<ContributionDocumentJson>
-  > = {
-    idcc: "123",
-    questionName: "Question",
-    seoTitle: "SEO Title",
-    description: "Description",
-  };
+      it("doit générer les metadata sans prendre en compte les thèmes ou le titre SEO", () => {
+        const metadata: ContributionMetadata = generateMetadata(
+          [],
+          contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+          breadcrumbs
+        );
 
-  const breadcrumbs: Breadcrumb[] = [
-    {
-      label: "Breadcrumb 1",
-      position: 1,
-      slug: "slug1",
-    },
-    {
-      label: "Breadcrumb 2",
-      position: 2,
-      slug: "slug2",
-    },
-  ];
+        expect(metadata.title).toBe("Question");
+        expect(metadata.text).toBe("Description");
+        expect(metadata.metas.title).toBe("Question");
+        expect(metadata.metas.description).toBe("Description");
+      });
+    });
 
-  it("should generate metadata with title, text, metaTitle, and metaDescription", () => {
-    const metadata: ContributionMetadata = generateMetadata(
-      ccns as DocumentElasticWithSource<AgreementDoc>[],
-      contribution as DocumentElasticWithSource<ContributionDocumentJson>,
-      breadcrumbs
-    );
+    describe("sans titre SEO personnalisé sans prendre en compte les thèmes ou le titre SEO", () => {
+      const contribution: Partial<
+        DocumentElasticWithSource<ContributionDocumentJson>
+      > = {
+        idcc: "0000",
+        questionName: "Question",
+        seoTitle: "",
+        description: "Description",
+      };
 
-    expect(metadata.title).toBe("Question");
-    expect(metadata.text).toBe("Description");
-    expect(metadata.metas.title).toBe("SEO Title");
-    expect(metadata.metas.description).toBe("Question - Description");
+      it("doit générer les metadata", () => {
+        const metadata: ContributionMetadata = generateMetadata(
+          [],
+          contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+          breadcrumbs
+        );
+
+        expect(metadata.title).toBe("Question");
+        expect(metadata.text).toBe("Description");
+        expect(metadata.metas.title).toBe("Question");
+        expect(metadata.metas.description).toBe("Description");
+      });
+    });
+  });
+
+  describe("Page personnalisée", () => {
+    const ccns: Partial<DocumentElasticWithSource<AgreementDoc>>[] = [
+      {
+        num: 123,
+        shortTitle: "CC Short title",
+      },
+    ];
+
+    describe("avec un titre SEO personnalisé", () => {
+      const contribution: Partial<
+        DocumentElasticWithSource<ContributionDocumentJson>
+      > = {
+        idcc: "123",
+        questionName: "Question",
+        seoTitle: "SEO Title",
+        description: "Description",
+      };
+
+      it("doit générer les metadata avec le titre SEO", () => {
+        const metadata: ContributionMetadata = generateMetadata(
+          ccns as DocumentElasticWithSource<AgreementDoc>[],
+          contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+          breadcrumbs
+        );
+
+        expect(metadata.title).toBe("Question - CC Short title");
+        expect(metadata.text).toBe("Description");
+        expect(metadata.metas.title).toBe("SEO Title - CC Short title");
+        expect(metadata.metas.description).toBe("Question - Description");
+      });
+    });
+
+    describe("sans titre SEO personnalisé", () => {
+      const contribution: Partial<
+        DocumentElasticWithSource<ContributionDocumentJson>
+      > = {
+        idcc: "123",
+        questionName: "Question",
+        seoTitle: "",
+        description: "Description",
+      };
+
+      it("doit générer les metadata avec le thème de plus haut niveau", () => {
+        const metadata: ContributionMetadata = generateMetadata(
+          ccns as DocumentElasticWithSource<AgreementDoc>[],
+          contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+          breadcrumbs
+        );
+
+        expect(metadata.title).toBe("Question - CC Short title");
+        expect(metadata.text).toBe("Description");
+        expect(metadata.metas.title).toBe("Breadcrumb 2 - CC Short title");
+        expect(metadata.metas.description).toBe("Question - Description");
+      });
+    });
+  });
+
+  describe("Page personnalisée avec une question longue et nom de la CC long", () => {
+    const ccns: Partial<DocumentElasticWithSource<AgreementDoc>>[] = [
+      {
+        num: 123,
+        shortTitle: "Nom de CC trop long",
+      },
+    ];
+
+    const contribution: Partial<
+      DocumentElasticWithSource<ContributionDocumentJson>
+    > = {
+      idcc: "123",
+      questionName: "Question très longue avec plus de cinquante caractères ?",
+      seoTitle: "",
+      description: "Description",
+    };
+
+    it("doit générer un titre avec seulement la question (sans le nom de la convention collective)", () => {
+      const metadata: ContributionMetadata = generateMetadata(
+        ccns as DocumentElasticWithSource<AgreementDoc>[],
+        contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+        breadcrumbs
+      );
+
+      expect(metadata.title).toBe(
+        "Question très longue avec plus de cinquante caractères ?"
+      );
+    });
+  });
+
+  describe("Page personnalisée avec une question courte et nom de la CC long", () => {
+    const ccns: Partial<DocumentElasticWithSource<AgreementDoc>>[] = [
+      {
+        num: 123,
+        shortTitle: "Nom de CC trop long",
+      },
+    ];
+
+    const contribution: Partial<
+      DocumentElasticWithSource<ContributionDocumentJson>
+    > = {
+      idcc: "123",
+      questionName: "Question très courte ?",
+      seoTitle: "",
+      description: "Description",
+    };
+
+    it("doit générer un titre avec seulement la question (sans le nom de la convention collective)", () => {
+      const metadata: ContributionMetadata = generateMetadata(
+        ccns as DocumentElasticWithSource<AgreementDoc>[],
+        contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+        breadcrumbs
+      );
+
+      expect(metadata.title).toBe("Question très courte ?");
+    });
+  });
+
+  describe("Page personnalisée avec une question courte et nom de la CC court", () => {
+    const ccns: Partial<DocumentElasticWithSource<AgreementDoc>>[] = [
+      {
+        num: 123,
+        shortTitle: "CC court",
+      },
+    ];
+
+    const contribution: Partial<
+      DocumentElasticWithSource<ContributionDocumentJson>
+    > = {
+      idcc: "123",
+      questionName: "Question très courte ?",
+      seoTitle: "",
+      description: "Description",
+    };
+
+    it("doit générer un titre avec la question et le nom de la convention collective", () => {
+      const metadata: ContributionMetadata = generateMetadata(
+        ccns as DocumentElasticWithSource<AgreementDoc>[],
+        contribution as DocumentElasticWithSource<ContributionDocumentJson>,
+        breadcrumbs
+      );
+
+      expect(metadata.title).toBe("Question très courte ? - CC court");
+    });
   });
 });
--- targets/export-elasticsearch/src/ingester/contributions/generateMetadata.ts
+++ targets/export-elasticsearch/src/ingester/contributions/generateMetadata.ts
@@ -7,33 +7,89 @@ import {
 } from "@socialgouv/cdtn-types";
 
 export const generateMetadata = (
-  ccns: DocumentElasticWithSource<AgreementDoc>[],
+  agreements: DocumentElasticWithSource<AgreementDoc>[],
   contribution: DocumentElasticWithSource<ContributionDocumentJson>,
   breadcrumbs: Breadcrumb[]
 ): ContributionMetadata => {
-  const cc = ccns.find((v) => v.num === parseInt(contribution.idcc));
+  if (contribution.idcc === "0000") {
+    return generateGenericMetadata(contribution);
+  }
 
-  const titleWithQuestion =
-    cc?.shortTitle &&
-    cc.shortTitle.length > 14 &&
-    contribution.questionName.length > 50
-      ? `${contribution.questionName} - ${cc.shortTitle}`
-      : contribution.questionName;
+  const agreement = agreements.find(
+    (v) => v.num === parseInt(contribution.idcc)
+  );
 
-  const metaTitle =
-    contribution.seoTitle ??
-    (breadcrumbs.length > 0 && cc?.shortTitle
-      ? `${breadcrumbs[breadcrumbs.length - 1].label} - ${cc.shortTitle}`
-      : titleWithQuestion);
+  if (!agreement) {
+    throw new Error(
+      `Can't find the agreement (${contribution.idcc}) information for contribution ${contribution.questionIndex} - ${contribution.questionName} (${contribution.id})`
+    );
+  }
 
-  const metaDescription = `${contribution.questionName} - ${contribution.description}`;
+  if (breadcrumbs.length === 0) {
+    throw new Error(
+      `Contribution ${contribution.questionIndex} - ${contribution.questionName} (${contribution.id}) must be themed`
+    );
+  }
 
+  return generateCustomMetadata(
+    contribution,
+    breadcrumbs[breadcrumbs.length - 1],
+    agreement
+  );
+};
+
+const generateGenericMetadata = (
+  contribution: DocumentElasticWithSource<ContributionDocumentJson>
+): ContributionMetadata => {
   return {
-    title: titleWithQuestion,
-    text: contribution.description, // champ qui est indexé par elasticsearch
+    title: contribution.questionName,
+    text: contribution.description,
     metas: {
-      title: metaTitle,
-      description: metaDescription,
+      title: contribution.questionName,
+      description: contribution.description,
     },
   };
+};
+
+const generateCustomMetadata = (
+  contribution: DocumentElasticWithSource<ContributionDocumentJson>,
+  breadcrumb: Breadcrumb,
+  agreement: DocumentElasticWithSource<AgreementDoc>
+): ContributionMetadata => {
+  return {
+    title: generateTitle(contribution.questionName, agreement.shortTitle),
+    text: contribution.description,
+    metas: {
+      title: generateMetaTitle(
+        contribution.seoTitle,
+        breadcrumb,
+        agreement.shortTitle
+      ),
+      description: `${contribution.questionName} - ${contribution.description}`,
+    },
+  };
+};
+const generateTitle = (
+  questionName: string,
+  agreementShortTitle: string | undefined
+) => {
+  if (
+    !agreementShortTitle ||
+    agreementShortTitle.length > 14 ||
+    questionName.length > 50
+  ) {
+    return questionName;
+  }
+  return `${questionName} - ${agreementShortTitle}`;
+};
+
+const generateMetaTitle = (
+  seoTitle: string | undefined,
+  breadcrumb: Breadcrumb,
+  agreementShortTitle: string
+): string => {
+  if (seoTitle && seoTitle !== "") {
+    return `${seoTitle} - ${agreementShortTitle}`;
+  }
+  return `${breadcrumb.label} - ${agreementShortTitle}`;
 };
