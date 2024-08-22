c80fa431701bce8298a8ca81e087dbcc0a4cbafb
maxgfr <25312957+maxgfr@users.noreply.github.com>
2024-08-22 10:29:10 +0200

    fix: down sql

--- targets/hasura/migrations/default/1723964911747_alter_table_model_models_add_column_display_date/down.sql
+++ targets/hasura/migrations/default/1723964911747_alter_table_model_models_add_column_display_date/down.sql
@@ -1,4 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "model"."models" add column "display_date" date
---  null;
+alter table "model"."models" drop column "display_date";
