cc09eb8f6e73c14abb8f679414bd882cee9171dd
carolineBda <caroline.bourdeu@gmail.com>
2024-09-26 13:39:47 +0200

    ajout des migrations pour mettre à jour les anciens utilisateurs

---
+++ targets/hasura/migrations/default/1727349866123_put_back_users_initials/up.sql
@@ -0,0 +1,15 @@
+UPDATE auth.users
+SET name = 'CL'
+WHERE id = '62a6e4c1-2624-4dfb-b984-0d206ec8a43e';
+
+UPDATE auth.users
+SET name = 'FD'
+WHERE id = '474702f8-7f7e-4f05-92b2-45796a075e0f';
+
+UPDATE auth.users
+SET name = 'AB'
+WHERE id = '1baef8d6-e871-46b4-8150-1f587b9f56cd';
+
+UPDATE auth.users
+SET name = 'CL'
+WHERE id = 'aa6d1721-71e5-42a8-bf26-98f453d1fab5';
