1dd1d025f1fb506dc1ec8ce432069c787328f65e
victor <victor.zeinstra@gmail.com>
2024-10-02 13:42:36 +0200

    chore: review

--- targets/alert-cli/src/dares/getAgreementsData.ts
+++ targets/alert-cli/src/dares/getAgreementsData.ts
@@ -28,7 +28,12 @@ export async function getAgreements(): Promise<GetAgreementData[]> {
   const result = await client
     .query<GetAgreementQueryResult>(getAgreementQuery, {})
     .toPromise();
-  if (!result || !result.data || !result.data?.agreements) {
+  if (
+    !result ||
+    !result.data ||
+    !result.data?.agreements ||
+    !result.data?.agreements.length
+  ) {
     throw new Error("An error occured on hasura agreement loading");
   }
   return result.data.agreements.map(({ name, id }) => {
