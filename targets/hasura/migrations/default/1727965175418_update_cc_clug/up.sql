526e36d228aa0cce640792d009d7703035d67421
carolineBda <caroline.bourdeu@gmail.com>
2024-10-03 16:21:17 +0200

    add migration

---
+++ targets/hasura/migrations/default/1727965175418_update_cc_clug/up.sql
@@ -0,0 +1,7 @@
+UPDATE public.documents
+SET slug = '2707-batiment-etam-ile-de-france-hors-seine-et-marne'
+WHERE slug = 'batiment-etam-ile-de-france-hors-seine-et-marne';
+
+UPDATE public.documents
+SET slug = '3251-bijouterie-joaillerie-orfevrerie-horlogerie-bjoh'
+WHERE slug = 'bijouterie-joaillerie-orfevrerie-horlogerie-bjoh';
