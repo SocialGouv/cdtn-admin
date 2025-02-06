658cdbf45baff3e147d24a18585539b3475f10e6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-02-06 16:00:23 +0100

    fix: deployment

--- shared/utils/src/gqlClient.ts
+++ shared/utils/src/gqlClient.ts
@@ -19,7 +19,6 @@ export const gqlDefaultProps: GqlClientParameter = {
 
 export const gqlClient = (props = gqlDefaultProps) =>
   createClient({
-    fetch: fetch,
     fetchOptions: {
       headers: {
         "Content-Type": "application/json",
