9e5ab71f35ceb3e04db8eeb97b63be42671efc41
Martial Maillot <martial.maillot@gmail.com>
2025-05-23 08:41:47 +0200

    fix: support des tableaux dans le type KeysToCamelCase

--- shared/types/src/utility.ts
+++ shared/types/src/utility.ts
@@ -3,9 +3,11 @@ type Camelize<T extends string> = T extends `${infer A}_${infer B}`
   : T;
 
 export type KeysToCamelCase<T> = {
-  [K in keyof T as Camelize<string & K>]: T[K] extends {}
-    ? KeysToCamelCase<T[K]>
-    : T[K];
+  [K in keyof T as Camelize<string & K>]: T[K] extends (infer U)[]
+    ? KeysToCamelCase<U>[]
+    : T[K] extends {}
+      ? KeysToCamelCase<T[K]>
+      : T[K];
 };
 
 export type Audience = "associations" | "particuliers" | "professionnels"
