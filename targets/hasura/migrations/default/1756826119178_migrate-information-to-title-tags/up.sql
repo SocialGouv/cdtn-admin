464a413937b9482580c236462d41f6f8e96aa0f2
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-09-05 11:10:29 +0200

    fix: convertir les tag hn vers title et sub-title (#1591)

---
+++ targets/hasura/migrations/default/1756826119178_migrate-information-to-title-tags/up.sql
@@ -0,0 +1,26 @@
+UPDATE information.informations_contents_blocks
+SET content = regexp_replace(
+    regexp_replace(
+        regexp_replace(
+            regexp_replace(content, '<h3([^>]*)>', '<span class="title"\1>', 'gi'),
+            '</h3>', '</span>', 'gi'
+        ),
+        '<h4([^>]*)>', '<span class="sub-title"\1>', 'gi'
+    ),
+    '</h4>', '</span>', 'gi'
+);
+
+UPDATE documents
+SET document = document || jsonb_build_object(
+    'content',
+    regexp_replace(
+        regexp_replace(
+            regexp_replace(
+                regexp_replace(document ->> 'content', '<h3([^>]*)>', '<span class="title"\1>', 'gi'),
+                '</h3>', '</span>', 'gi'
+            ),
+            '<h4([^>]*)>', '<span class="sub-title"\1>', 'gi'
+        ),
+        '</h4>', '</span>', 'gi'
+    )
+);
