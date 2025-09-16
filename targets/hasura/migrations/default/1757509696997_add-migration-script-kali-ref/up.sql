84b1784d4762614302e3e92da493e0fea7cd0563
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-09-16 10:28:30 +0200

    fix: query

--- targets/hasura/migrations/default/1757509696997_add-migration-script-kali-ref/up.sql
+++ targets/hasura/migrations/default/1757509696997_add-migration-script-kali-ref/up.sql
@@ -1,9 +1,13 @@
-with list as (
-SELECT aor.id as aorid, aor.answer_id, aor.url, aor."label", ka.id as kaid
-FROM contribution.answer_other_references aor
-inner JOIN kali_articles ka
-    ON REGEXP_REPLACE(SPLIT_PART(aor.url, '/', -1), '\?.*', '') = ka.id
-WHERE aor.url LIKE '%KALIART%'
+with other_ref as (
+    SELECT aor.id as id, aor.answer_id,  aor.url, REGEXP_REPLACE(SPLIT_PART(SPLIT_PART(aor.url, '/', 6), '#', 1), '\?.*', '') as kaidgen, aor."label"
+    FROM contribution.answer_other_references aor
+    WHERE aor.url LIKE '%KALIART%'
+),
+list as (
+    SELECT aor.id as aorid, aor.answer_id, aor.url, aor."label", ka.id as kaid
+    FROM other_ref aor
+    inner JOIN kali_articles ka
+        ON aor.kaidgen = ka.cid or aor.kaidgen = ka.id
 ),
 inserted as (
     INSERT INTO contribution.answer_kali_references (answer_id, article_id, "label")
