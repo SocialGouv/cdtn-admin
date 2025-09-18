6a6797d412d062e489862e9f20654ee59eb252b9
Victor Zeinstra <zeinstra.victor@gmail.com>
2025-09-18 14:12:30 +0200

    feat: ajout du script de migration des other ref vers kali ref (#1594)
    * feat: ajout du script de migration des other ref vers kali ref
    
    * fix: query
    
    * fix: query

---
+++ targets/hasura/migrations/default/1757509696997_add-migration-script-kali-ref/up.sql
@@ -0,0 +1,31 @@
+with other_ref as (
+    SELECT aor.id as id, aor.answer_id,  aor.url, REGEXP_REPLACE(SPLIT_PART(SPLIT_PART(aor.url, '/', 6), '#', 1), '\?.*', '') as kaidgen, aor."label"
+    FROM contribution.answer_other_references aor
+    WHERE aor.url LIKE '%KALIART%'
+),
+list as (
+    SELECT aor.id as aorid, aor.answer_id, aor.url, aor."label", ka.id as kaid
+    FROM other_ref aor
+    inner JOIN kali_articles ka
+        ON aor.kaidgen = ka.cid or aor.kaidgen = ka.id
+    where not exists(
+        select 1
+        from contribution.answer_kali_references akr
+        where akr.answer_id = aor.answer_id
+        and akr.article_id = aor.kaidgen
+    )
+),
+inserted as (
+    INSERT INTO contribution.answer_kali_references (answer_id, article_id, "label")
+    SELECT answer_id, kaid, "label"
+    FROM list
+    RETURNING id
+),
+deleted as (
+    DELETE FROM contribution.answer_other_references
+    WHERE id IN (SELECT aorid FROM list)
+    RETURNING id
+)
+select id, 'inserted' as status from inserted
+union
+select id, 'deleted' as status from deleted;
