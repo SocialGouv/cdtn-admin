9f6b86088393b6dc5883ab0a018f834377bc2364
Martial Maillot <martial.maillot@gmail.com>
2026-09-22 08:41:27 +0200

    feat(outils): simulateur d'indemnité de départ à la retraite

---
+++ targets/hasura/migrations/default/1783598367002_insert_indemnite_retraite/up.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.documents (cdtn_id, initial_id, title, meta_description, source, slug, "text", "document",
+                              is_published, is_searchable, is_available)
+VALUES ('af4bc7be81', '99250f5e-899f-486d-97b6-4add2fdc1f5b', 'Indemnité de départ ou mise à la retraite',
+        'Vous souhaitez calculer le montant de l''indemnité de départ ou mise à la retraite ? Notre simulateur vous apporte une réponse personnalisée.',
+        'outils', 'indemnite-retraite',
+        'Quelle est mon indemnité de départ ou mise à la retraite\nComment calculer mon indemnité de départ ou mise à la retraite\ncalculez le montant d’une indemnité de départ ou mise à la retraite en fonction de votre situation',
+        '{"date": "01/06/2024", "icon": "Rupture", "order": 0, "action": "Estimer", "metaTitle": "Simulateur - Calcul de l''indemnité de départ ou mise à la retraite", "questions": ["Quelle est mon indemnité de départ ou mise à la retraite", "Comment calculer mon indemnité de départ ou mise à la retraite", "calculez le montant d''une indemnité de départ ou mise à la retraite en fonction de votre situation"], "description": "Estimez simplement le montant de l’indemnité de départ ou mise à la retraite", "displayTool": false, "displayTitle": "Calculer l''indemnité de départ ou mise à la retraite"}',
+        true, true, true);
